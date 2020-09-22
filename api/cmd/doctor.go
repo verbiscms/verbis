d4f0daf58b2cdb43e55bd0c0d0e0457852232eac
Ainsley Clark <info@ainsleyclark.com>
2020-09-22 20:11:58 +0100

    Fixed install
    Former-commit-id: d0388f8f895179d88f97eb852709ef1ee3293bfa [formerly f2a4d5e2c46074d5d8dd4324798c96357b1ff68e]
    Former-commit-id: 65c745d236f0c9e70d1c14192a25e2731c841e4f

--- api/cmd/doctor.go
+++ api/cmd/doctor.go
@@ -15,45 +15,50 @@ var (
 		Long:  `This command is a diagnostic tool to find any potential issues for your
 Verbis install. It will check if the database has been set up correctly as well as the
 environment.`,
-		RunE: func(cmd *cobra.Command, args []string) error {
-
-			// TODO:
-			// This needs to be a function that returns an error and let cobra handle the errors
-			// No lowercase
-
-			// Load the environment (.env file)
-			err := environment.Load()
-			if err != nil {
-				return fmt.Errorf(err.Error())
+		Run: func(cmd *cobra.Command, args []string)  {
+			if err := doctor(); err != nil {
+				return
 			}
-
-			// Check if the environment values are valid
-			vErrors := environment.Validate()
-			if vErrors != nil {
-				for _, v := range vErrors {
-					var msg = fmt.Sprintf("-> Error obtaining environment variable: %s", strings.ToUpper(v.Key))
-					if v.Type == "ip" {
-						msg = fmt.Sprintf("-> Error obtaining environment variable: %s must be a valid IP address", strings.ToUpper(v.Key))
-					}
-					color.Red.Printf(msg)
-					fmt.Println()
-				}
-				return fmt.Errorf("Validation failed for the enviroment")
-			}
-
-			// Get the database and ping
-			if _, err := app.db.GetDatabase(); err != nil {
-				color.Red.Println("-> Error establishing database connection, are the credentials in the .env file correct?")
-				return fmt.Errorf("Error establishing database connection")
-			}
-
-			// Check if the database exists
-			if err := app.db.CheckExists(); err != nil {
-				color.Red.Println("-> Error establishing database connection, are the credentials in the .env file correct?")
-				return fmt.Errorf("Error establishing database connection")
-			}
-
-			return nil
 		},
 	}
 )
+
+func doctor() error {
+	// TODO:
+	// This needs to be a function that returns an error and let cobra handle the errors
+	// No lowercase
+
+	// Load the environment (.env file)
+	err := environment.Load()
+	if err != nil {
+		return fmt.Errorf(err.Error())
+	}
+
+	// Check if the environment values are valid
+	vErrors := environment.Validate()
+	if vErrors != nil {
+		for _, v := range vErrors {
+			var msg = fmt.Sprintf("-> Error obtaining environment variable: %s", strings.ToUpper(v.Key))
+			if v.Type == "ip" {
+				msg = fmt.Sprintf("-> Error obtaining environment variable: %s must be a valid IP address", strings.ToUpper(v.Key))
+			}
+			color.Red.Printf(msg)
+			fmt.Println()
+		}
+		return fmt.Errorf("Validation failed for the enviroment")
+	}
+
+	// Get the database and ping
+	if _, err := app.db.GetDatabase(); err != nil {
+		color.Red.Println("-> Error establishing database connection, are the credentials in the .env file correct?")
+		return fmt.Errorf("Error establishing database connection")
+	}
+
+	// Check if the database exists
+	if err := app.db.CheckExists(); err != nil {
+		color.Red.Println("-> Error establishing database connection, are the credentials in the .env file correct?")
+		return fmt.Errorf("Error establishing database connection")
+	}
+
+	return nil
+}
