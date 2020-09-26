dd5d9969506f08df4d99506cc613660dac8c2ffd
Ainsley Clark <ainsley@reddico.co.uk>
2020-09-26 20:59:23 +0100

    Error formatting

--- api/helpers/encryption/password.go
+++ api/helpers/encryption/password.go
@@ -1,17 +1,21 @@
 package encryption
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"fmt"
+	"github.com/ainsleyclark/verbis/api/errors"
+	"golang.org/x/crypto/bcrypt"
+)
 
-// HashPassword
+// HashPassword gets the password in byte format and generates
+// a hashed password with the default cost of 10.
+// Returns errors.INTERNAL if the bcrypt failed to generate
+// from password.
 func HashPassword(password string) (string, error) {
-	// Get current password in byte.
+	const op = "encryption.HashPassword"
 	bytePassword := []byte(password)
-
-	// Hashing the password with the default cost of 10.
 	hashedPassword, err := bcrypt.GenerateFromPassword(bytePassword, bcrypt.DefaultCost)
 	if err != nil {
-		return "", err
+		return "", &errors.Error{Code: errors.INTERNAL, Message: fmt.Sprintf("Could has the password with the string: %s", password), Operation: op, Err: err}
 	}
-
 	return string(hashedPassword), err
 }
