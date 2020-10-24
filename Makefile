a5b999ac3aa23790b3ea9716364a1db2e3ee3bb0
Ainsley Clark <info@ainsleyclark.com>
2020-10-24 18:07:54 +0100

    Update
    Former-commit-id: b02ece46dcd08eb148a8ad78d58bc5dfeaa95b62 [formerly ed1bb106ba6a563e7759673b93d5ebfc9b7c41c3]
    Former-commit-id: a48c3c16e8febf8a65218f6438cb74888a87ae98

--- Makefile
+++ Makefile
@@ -16,5 +16,8 @@ live-serve:
 live-test:
 	HOST="localhost" gin -i --port=8080 --laddr=127.0.0.1 --all run test
 
+mock:
+	mockgen -destination=api/mocks/mock_store.go -package=mocks github.com/ainsleyclark/verbis/api/models AuthRepository,CategoryRepository,FieldsRepository,MediaRepository,OptionsRepository,PostsRepository,RoleRepository,SeoMetaRepository,SessionRepository,SiteRepository,UserRepository
+
 install:
 	go install cms
--- api/templates/fields.go
+++ api/templates/fields.go
@@ -1,44 +1,71 @@
 package templates
 
-type FieldsHandler interface {
-	getField(field string) interface{}
+import (
+	"encoding/json"
+	"github.com/ainsleyclark/verbis/api/models"
+)
+
+type fieldsHandler interface {
+	getField(field string, id ...int) interface{}
 	getFields() map[string]interface{}
 	hasField(field string) bool
 	getRepeater(field string) []map[string]interface{}
 	getFlexible(field string) []map[string]interface{}
-	getSubField(field string) map[string]interface{}
+	getSubField(field string, layout map[string]interface{}) interface{}
 }
 
-type Fields struct {
+type fields struct {
 	fields map[string]interface{}
+	store *models.Store
 }
 
 // newFields - Construct
-func newFields(f map[string]interface{}) *Fields {
-	return &Fields{
+func newFields(f map[string]interface{}, s *models.Store) *fields {
+	return &fields{
 		fields: f,
+		store: s,
 	}
 }
 
+// If field is type of media
+// If field is type of post
+// Get the field
+
 // getField - Get field based on input return nothing if found
-func (f *Fields) getField(field string) interface{} {
-	if _, found := f.fields[field]; found {
-		return f.fields[field]
-	} else {
+func (f *fields) getField(field string, id ...int) interface{} {
+
+	// Check if the post ID was passed, and assign
+	fields := f.fields
+	if len(id) > 0 {
+		post, err := f.store.Posts.GetById(id[0])
+
+		if err != nil {
+			return ""
+		}
+
+		var m map[string]interface{}
+		if err := json.Unmarshal(*post.Fields, &m); err != nil {
+			return ""
+		}
+
+		fields = m
+	}
+
+	val, found := fields[field]
+	if !found {
 		return ""
 	}
 
-	// If field is type of media
-	// If field is type of post
+	return val
 }
 
 // getFields - Get all fields for template
-func (f *Fields) getFields() map[string]interface{} {
+func (f *fields) getFields() map[string]interface{} {
 	return f.fields
 }
 
 // hasField - Determine if the given field exists
-func (f *Fields) hasField(field string) bool {
+func (f *fields) hasField(field string) bool {
 	if _, found := f.fields[field]; found {
 		return true
 	}
@@ -46,7 +73,7 @@ func (f *Fields) hasField(field string) bool {
 }
 
 // getRepeater
-func (f *Fields) getRepeater(field string) []map[string]interface{} {
+func (f *fields) getRepeater(field string) []map[string]interface{} {
 	if _, found := f.fields[field]; found {
 		fields := f.fields[field].([]interface{})
 		var f []map[string]interface{}
@@ -59,7 +86,7 @@ func (f *Fields) getRepeater(field string) []map[string]interface{} {
 }
 
 // getFlexible
-func (f *Fields) getFlexible(field string) []map[string]interface{} {
+func (f *fields) getFlexible(field string) []map[string]interface{} {
 	if _, found := f.fields[field]; found {
 		fields, ok := f.fields[field].([]interface{})
 		if !ok {
@@ -74,7 +101,7 @@ func (f *Fields) getFlexible(field string) []map[string]interface{} {
 	return nil
 }
 
-func (f *Fields) getSubField(field string, layout map[string]interface{}) interface{} {
+func (f *fields) getSubField(field string, layout map[string]interface{}) interface{} {
 	block := layout["fields"]
 	fields, ok := block.(map[string]interface{})
 	if !ok {
