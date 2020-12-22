4570eb458c8c93808e1d22563574d54892fbde3f
Ainsley Clark <info@ainsleyclark.com>
2020-12-22 19:12:58 +0000

    Partial test
    Former-commit-id: 4215d6c88f68d679879d6d512b1872bd9e44bc32 [formerly 6106cca667d4877a1104393825b66e3258685d19]
    Former-commit-id: 54b66822fd57b0ad2b7cf9ec1275479bc01743e5

--- api/templates/media.go
+++ api/templates/media.go
@@ -4,7 +4,9 @@ import (
 	"github.com/ainsleyclark/verbis/api/domain"
 )
 
-// getMedia obtains the media by ID and returns a domain.Media type
+// getMedia
+//
+// Obtains the media by ID and returns a domain.Media type
 // or nil if not found.
 func (t *TemplateFunctions) getMedia(i interface{}) *domain.Media {
 	var id int
