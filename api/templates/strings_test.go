8b26deb3faa50de1a76dacdd7ea77cd5793bb47a
Ainsley Clark <info@ainsleyclark.com>
2020-12-23 16:42:11 +0000

    Updatemake
    Former-commit-id: 157cc48772c2c9c4e25500a215090ad5d0e50300 [formerly d5f9af9a8a98c67227dcf41d76239147bab57a37]
    Former-commit-id: c5f88b8a834cf17d7c3ba6dd21dd19a98e014c9b

---
+++ api/templates/strings_test.go
@@ -0,0 +1,135 @@
+package templates
+
+import "testing"
+
+func Test_Len(t *testing.T) {
+
+	tt := map[string]struct {
+		tmpl string
+		want interface{}
+	}{
+		"Valid": {
+			tmpl: `{{ len "verbis" }}`,
+			want: "6",
+		},
+		"Valid 2": {
+			tmpl: `{{ len "verbis cms" }}`,
+			want: "10",
+		},
+		"Int": {
+			tmpl: `{{ len 1234 }}`,
+			want: 4,
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func(t *testing.T) {
+			f := newTestSuite()
+			runt(t, f, test.tmpl, test.want)
+		})
+	}
+}
+
+func Test_Replace(t *testing.T) {
+
+	tt := map[string]struct {
+		tmpl string
+		want interface{}
+	}{
+		"Valid": {
+			tmpl: `{{ replace "verbis-cms-is-amazing" "-" " " }}`,
+			want: "verbis cms is amazing",
+		},
+		"Valid 2": {
+			tmpl: `{{ replace "verbis" "v" "" }}`,
+			want: "erbis",
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func(t *testing.T) {
+			f := newTestSuite()
+			runt(t, f, test.tmpl, test.want)
+		})
+	}
+}
+
+func Test_Substr(t *testing.T) {
+
+	tt := map[string]struct {
+		tmpl string
+		want interface{}
+	}{
+		"Valid": {
+			tmpl: `{{ substr "verbiscms" 0 2 }}`,
+			want: "ve",
+		},
+		"Valid 2": {
+			tmpl: `{{ substr "hello world" 0 5 }}`,
+			want: "hello",
+		},
+		"Strings as Params": {
+			tmpl: `{{ substr "hello world" "0" "5" }}`,
+			want: "hello",
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func(t *testing.T) {
+			f := newTestSuite()
+			runt(t, f, test.tmpl, test.want)
+		})
+	}
+}
+
+func Test_Trunc(t *testing.T) {
+
+	tt := map[string]struct {
+		tmpl string
+		want interface{}
+	}{
+		"Positive": {
+			tmpl: `{{ trunc "hello world" 5 }}`,
+			want: "hello",
+		},
+		"Negative": {
+			tmpl: `{{ trunc "hello world" -5 }}`,
+			want: "world",
+		},
+		"Strings as Params": {
+			tmpl: `{{ trunc "hello world" "-5" }}`,
+			want: "world",
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func(t *testing.T) {
+			f := newTestSuite()
+			runt(t, f, test.tmpl, test.want)
+		})
+	}
+}
+
+func Test_Ellipsis(t *testing.T) {
+
+	tt := map[string]struct {
+		tmpl string
+		want interface{}
+	}{
+		"Valid": {
+			tmpl: `{{ ellipsis 5 "hello world" }}`,
+			want: "hello...",
+		},
+		"Valid2": {
+			tmpl: `{{ ellipsis 11 "hello world this is Verbis CMS" }}`,
+			want: "hello world...",
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func(t *testing.T) {
+			f := newTestSuite()
+			runt(t, f, test.tmpl, test.want)
+		})
+	}
+}
