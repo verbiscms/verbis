c15eae10d867a31633597c36fcc98d40aacd0490
Ainsley Clark <ainsley@reddico.co.uk>
2020-12-28 16:44:07 +0000

    Util functions
    Former-commit-id: a9ed971adfbc1aac324d081d4d074c6fe3b5cd16 [formerly cda167d51502a2481b044c414ab73d87d6d3ea59]
    Former-commit-id: 323dcdcf0f498140160f18c10f056efd48190fb8

--- api/templates/util_test.go
+++ api/templates/util_test.go
@@ -8,31 +8,31 @@ func Test_Len(t *testing.T) {
 
 	tt := map[string]struct {
 		input interface{}
-		want interface{}
+		want  interface{}
 	}{
 		"Slice": {
-			input: []int{1,2,3,4,5},
-			want: 5,
+			input: []int{1, 2, 3, 4, 5},
+			want:  5,
 		},
 		"Slice Pointer": {
-			input: &[]int{1,2,3,4,5},
-			want: 5,
+			input: &[]int{1, 2, 3, 4, 5},
+			want:  5,
 		},
 		"Array": {
-			input: [5]int{1,2,3,4,5},
-			want: 5,
+			input: [5]int{1, 2, 3, 4, 5},
+			want:  5,
 		},
 		"Array Pointer": {
-			input: &[5]int{1,2,3,4,5},
-			want: 5,
+			input: &[5]int{1, 2, 3, 4, 5},
+			want:  5,
 		},
 		"String": {
 			input: "hello",
-			want: 5,
+			want:  5,
 		},
 		"String Pointer": {
 			input: &ptr,
-			want: 5,
+			want:  5,
 		},
 		"Map": {
 			input: map[string]string{
@@ -42,7 +42,7 @@ func Test_Len(t *testing.T) {
 		},
 		"Error": {
 			input: 123,
-			want: 0,
+			want:  0,
 		},
 	}
 
@@ -57,38 +57,38 @@ func Test_Explode(t *testing.T) {
 
 	tt := map[string]struct {
 		delim interface{}
-		text interface{}
-		want interface{}
+		text  interface{}
+		want  interface{}
 	}{
 		"Spaces": {
 			delim: " ",
-			text: "hello world !",
-			want: "[hello world !]",
+			text:  "hello world !",
+			want:  "[hello world !]",
 		},
 		"Commas": {
 			delim: ",",
-			text: "hello,world,!",
-			want: "[hello world !]",
+			text:  "hello,world,!",
+			want:  "[hello world !]",
 		},
 		"Int": {
 			delim: "",
-			text: 123,
-			want: "[1 2 3]",
+			text:  123,
+			want:  "[1 2 3]",
 		},
 		"No Stringer Delim": {
 			delim: noStringer{},
-			text: "hello,world,!",
-			want: "[]",
+			text:  "hello,world,!",
+			want:  "[]",
 		},
 		"No Stringer Text": {
 			delim: " ",
-			text: noStringer{},
-			want: "[]",
+			text:  noStringer{},
+			want:  "[]",
 		},
 		"Length": {
 			delim: "hello",
-			text: ",",
-			want: "[hello]",
+			text:  ",",
+			want:  "[hello]",
 		},
 	}
 
@@ -96,7 +96,7 @@ func Test_Explode(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			runtv(t, newTestSuite(), `{{ explode .Delim .Text }}`, test.want, map[string]interface{}{
 				"Delim": test.delim,
-				"Text": test.text,
+				"Text":  test.text,
 			})
 		})
 	}
@@ -105,41 +105,41 @@ func Test_Explode(t *testing.T) {
 func Test_Implode(t *testing.T) {
 
 	tt := map[string]struct {
-		glue interface{}
+		glue  interface{}
 		slice interface{}
-		want interface{}
+		want  interface{}
 	}{
 		"Spaces": {
-			glue: " ",
+			glue:  " ",
 			slice: []string{"a", "b", "c"},
-			want: "a b c",
+			want:  "a b c",
 		},
 		"Commas": {
-			glue: ",",
+			glue:  ",",
 			slice: []string{"a", "b", "c"},
-			want: "a,b,c",
+			want:  "a,b,c",
 		},
 		"Int": {
-			glue: ",",
-			slice: []int{1,2,3},
-			want: "1,2,3",
+			glue:  ",",
+			slice: []int{1, 2, 3},
+			want:  "1,2,3",
 		},
 		"No Stringer Glue": {
-			glue: noStringer{},
+			glue:  noStringer{},
 			slice: []string{"a", "b", "c"},
-			want: "",
+			want:  "",
 		},
 		"No Stringer Slice": {
-			glue: " ",
+			glue:  " ",
 			slice: noStringer{},
-			want: "",
+			want:  "",
 		},
 	}
 
 	for name, test := range tt {
 		t.Run(name, func(t *testing.T) {
 			runtv(t, newTestSuite(), `{{ implode .Glue .Slice }}`, test.want, map[string]interface{}{
-				"Glue": test.glue,
+				"Glue":  test.glue,
 				"Slice": test.slice,
 			})
 		})
