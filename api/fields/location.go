6e339b114dbf532f14ddcd7a0bd41ea7ef94e763
Ainsley Clark <ainsley@reddico.co.uk>
2020-12-31 17:58:22 +0000

    Tidied up location
    Former-commit-id: f2b08b3ac1fc70bfd77400192209e7c2e7d1c099 [formerly b22858e63a712540925815c1adb8aad5c4b5fa06]
    Former-commit-id: 6d0fa5aa0a53fdd7702774c34cc76161252e52f6

---
+++ api/fields/location.go
@@ -0,0 +1,244 @@
+package fields
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/ainsleyclark/verbis/api/cache"
+	"github.com/ainsleyclark/verbis/api/domain"
+	"github.com/ainsleyclark/verbis/api/errors"
+	"github.com/ainsleyclark/verbis/api/helpers/paths"
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+// Location defines
+type Location struct {
+	// Groups defines the current field groups that
+	// are saved to disk in storage.
+	Groups []domain.FieldGroup
+	// jsonPath defines where JSON files containing
+	// domain.FieldGroups are kept
+	JsonPath string
+}
+
+// NewLocation - Construct
+func NewLocation() *Location {
+	l := &Location{
+		JsonPath: paths.Storage() + "/fields",
+	}
+
+	fg, err := l.GetFieldGroups()
+	if err != nil {
+		log.WithFields(log.Fields{"error": err}).Error()
+	}
+
+	return &Location{
+		Groups: fg,
+	}
+}
+
+// GetLayout
+//
+// Loops over all of the locations within the config json
+// file that is defined. Produces an array of field groups that
+// can be returned for the post
+//
+// Returns
+func (l *Location) GetLayout(p domain.Post, a domain.User, c *domain.Category, cacheable bool) []domain.FieldGroup {
+
+	// If the cache allows for caching of layouts & if the
+	// layout has already been cached, return.
+	var found bool
+	if cacheable {
+		cached, found := cache.Store.Get("field_layout_" + p.UUID.String())
+		if found {
+			return cached.([]domain.FieldGroup)
+		}
+	}
+
+	// Get the groups from the resolver
+	groups := l.groupResolver(p, a, c)
+
+	// Set the cache field layout if the cache was not found
+	if !found && cacheable {
+		cache.Store.Set("field_layout_"+p.UUID.String(), &groups, cache.RememberForever)
+	}
+
+	return groups
+}
+
+// GroupResolver
+//
+// Loops over all of the locations within the config json
+// file that is defined. Produces an array of field groups that
+// can be returned for the post
+//
+// Returns
+func (l *Location) groupResolver(p domain.Post, a domain.User, c *domain.Category) []domain.FieldGroup {
+
+	var fg []domain.FieldGroup
+
+	// Loop over the groups
+	for _, group := range l.Groups {
+
+		// Check for empty locationjson
+		if len(group.Locations) == 0 && !hasBeenAdded(group.UUID.String(), fg) {
+			fg = append(fg, group)
+
+		} else {
+
+			// Check and Loop over locations
+			for _, location := range group.Locations {
+
+				if !hasBeenAdded(group.UUID.String(), fg) {
+
+					// Loop over rule sets
+					var locationSet []bool
+					for _, rule := range location {
+
+						switch rule.Param {
+						case "status":
+							locationSet = append(locationSet, checkLocation(p.Status, rule))
+							break
+						case "post":
+							locationSet = append(locationSet, checkLocation(strconv.Itoa(p.Id), rule))
+							break
+						case "page_template":
+							locationSet = append(locationSet, checkLocation(p.PageTemplate, rule))
+							break
+						case "page_layout":
+							locationSet = append(locationSet, checkLocation(p.PageLayout, rule))
+							break
+						case "resource":
+							if p.Resource != nil {
+								locationSet = append(locationSet, checkLocation(*p.Resource, rule))
+							} else {
+								locationSet = append(locationSet, false)
+							}
+							break
+						case "category":
+							if c != nil {
+								locationSet = append(locationSet, checkLocation(strconv.Itoa(c.Id), rule))
+							} else {
+								locationSet = append(locationSet, false)
+							}
+							break
+						case "author":
+							locationSet = append(locationSet, checkLocation(strconv.Itoa(p.UserId), rule))
+							break
+						case "role":
+							locationSet = append(locationSet, checkLocation(strconv.Itoa(a.Role.Id), rule))
+							break
+						}
+					}
+
+					// Remove from the array for the front end
+					group.Locations = nil
+
+					if checkMatch(locationSet) {
+						fg = append(fg, group)
+					}
+				}
+			}
+		}
+	}
+
+	// Append empty if nil
+	if fg == nil {
+		fg = []domain.FieldGroup{}
+	}
+
+	return fg
+}
+
+// GetFieldGroups
+//
+// This function will loop over all of the json files that have been
+// stored in /storage/fields and append them to the array to be
+// returned.
+//
+// Returns errors.INTERNAL if the path file not be read or be unmarshalled
+func (l *Location) GetFieldGroups() ([]domain.FieldGroup, error) {
+	const op = "Fields.GetFieldGroups"
+
+	var fg []domain.FieldGroup
+	err := filepath.Walk(l.JsonPath, func(path string, info os.FileInfo, err error) error {
+		if strings.Contains(info.Name(), "json") {
+
+			file, err := ioutil.ReadFile(path)
+			if err != nil {
+				return &errors.Error{Code: errors.INTERNAL, Message: fmt.Sprintf("Unable to read field file with the path: %s", path), Operation: op, Err: err}
+			}
+
+			var fields domain.FieldGroup
+			err = json.Unmarshal(file, &fields)
+			if err != nil {
+				return &errors.Error{Code: errors.INTERNAL, Message: "Unable to unmarshal the field struct", Operation: op, Err: err}
+			}
+
+			fg = append(fg, fields)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return fg, nil
+}
+
+// checkLocation
+//
+// Checks to see if there has been a match within the location
+// json passed, the string could be resource. page
+// template or anything defined within fields.
+func checkLocation(check string, location domain.FieldLocation) bool {
+	var match = false
+
+	switch location.Operator {
+	case "==":
+		if check == location.Value {
+			match = true
+		}
+	case "!=":
+		if check != location.Value {
+			match = true
+		}
+	}
+
+	return match
+}
+
+// checkMatch
+//
+// Checks to see if the there has been a match within
+// the locationjson block by using an array of booleans, if there
+// has already been a match, it will return false. Useful
+// for and locationjson blocks not or.
+func checkMatch(matches []bool) bool {
+	for _, a := range matches {
+		if !a {
+			return false
+		}
+	}
+	return true
+}
+
+// hasBeenAdded
+//
+// Checks to see if there already been a match within
+// the array of field groups by comparing key and
+// the UUID.
+func hasBeenAdded(key string, fg []domain.FieldGroup) bool {
+	for _, v := range fg {
+		if v.UUID.String() == key {
+			return true
+		}
+	}
+	return false
+}
--- api/models/fields.go
+++ api/models/fields.go
@@ -1,30 +1,22 @@
 package models
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/ainsleyclark/verbis/api/cache"
 	"github.com/ainsleyclark/verbis/api/config"
-	log "github.com/sirupsen/logrus"
-
+	"github.com/ainsleyclark/verbis/api/fields"
 	//"github.com/ainsleyclark/verbis/api/cache"
 	"github.com/ainsleyclark/verbis/api/domain"
 	"github.com/ainsleyclark/verbis/api/errors"
-	"github.com/ainsleyclark/verbis/api/helpers/files"
 	"github.com/ainsleyclark/verbis/api/helpers/paths"
 	"github.com/jmoiron/sqlx"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
 )
 
 // FieldsRepository defines methods for Posts to interact with the database
 type FieldsRepository interface {
-	GetByPost(postId int) ([]domain.PostField, error)
-	GetByPostAndKey(key string, postId int) (domain.PostField, error)
-	GetFieldGroups() (*[]domain.FieldGroup, error)
-	GetLayout(p domain.Post, a domain.User, c *domain.Category) *[]domain.FieldGroup
+	//GetByPost(postId int) ([]domain.PostField, error)
+	//GetByPostAndKey(key string, postId int) (domain.PostField, error)
+	//GetFieldGroups() (*[]domain.FieldGroup, error)
+	GetLayout(p domain.Post, a domain.User, c *domain.Category) []domain.FieldGroup
 }
 
 // FieldsStore defines the data layer for Posts
@@ -70,207 +62,6 @@ func (s *FieldsStore) GetByPostAndKey(key string, postId int) (domain.PostField,
 // GetLayout loops over all of the locations within the config json
 // file that is defined. Produces an array of field groups that
 // can be returned for the post
-func (s *FieldsStore) GetLayout(p domain.Post, a domain.User, c *domain.Category) *[]domain.FieldGroup {
-	var fg []domain.FieldGroup
-
-	// If the cache allows for caching of layouts & if the
-	// layout has already been cached, return.
-	var found bool
-	if s.options.CacheServerFields {
-		cached, found := cache.Store.Get("field_layout_" + p.UUID.String())
-		if found {
-			return cached.(*[]domain.FieldGroup)
-		}
-	}
-
-	// Obtain json files
-	fieldGroups, err := s.GetFieldGroups()
-	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error()
-	}
-
-	// Loop over the groups
-	for _, group := range *fieldGroups {
-
-		// Check for empty location
-		if len(group.Locations) == 0 && !s.hasBeenAdded(group.UUID.String(), fg) {
-			fg = append(fg, group)
-
-		} else {
-
-			// Check and Loop over locations
-			for _, location := range group.Locations {
-
-				if !s.hasBeenAdded(group.UUID.String(), fg) {
-
-					// Loop over rule sets
-					var locationSet []bool
-					for _, rule := range location {
-
-						switch rule.Param {
-						// Status
-						case "status":
-							{
-								locationSet = append(locationSet, s.checkLocation(p.Status, rule))
-								break
-							}
-						// Status
-						case "post":
-							{
-								locationSet = append(locationSet, s.checkLocation(strconv.Itoa(p.Id), rule))
-								break
-							}
-						// Page Templates
-						case "page_template":
-							{
-								locationSet = append(locationSet, s.checkLocation(p.PageTemplate, rule))
-								break
-							}
-						// Layout
-						case "layout":
-							{
-								locationSet = append(locationSet, s.checkLocation(p.PageLayout, rule))
-								break
-							}
-						// Resources
-						case "resource":
-							{
-								if p.Resource != nil {
-									locationSet = append(locationSet, s.checkLocation(*p.Resource, rule))
-								} else {
-									locationSet = append(locationSet, false)
-								}
-								break
-							}
-						// Categories
-						case "categories":
-							{
-								if c != nil {
-									locationSet = append(locationSet, s.checkLocation(strconv.Itoa(c.Id), rule))
-								} else {
-									locationSet = append(locationSet, false)
-								}
-								break
-							}
-						// Author
-						case "author":
-							{
-								locationSet = append(locationSet, s.checkLocation(strconv.Itoa(p.UserId), rule))
-								break
-							}
-						// Role
-						case "role":
-							{
-								locationSet = append(locationSet, s.checkLocation(strconv.Itoa(a.Role.Id), rule))
-								break
-							}
-						}
-					}
-
-					// Remove from the array for the front end
-					group.Locations = nil
-
-					if s.checkMatch(locationSet) {
-						fg = append(fg, group)
-					}
-				}
-			}
-		}
-	}
-
-	// Append empty if nil
-	if fg == nil {
-		fg = []domain.FieldGroup{}
-	}
-
-	// Set the cache field layout if the cache was not found
-	if !found && s.options.CacheServerFields {
-		cache.Store.Set("field_layout_"+p.UUID.String(), &fg, cache.RememberForever)
-	}
-
-	return &fg
-}
-
-// GetFieldGroups will loop over all of the json files that have been
-// stored in /storage/fields and append them to the array to be
-// returned.
-// Returns errors.INTERNAL if the path file not be read or be unmarshalled
-func (s *FieldsStore) GetFieldGroups() (*[]domain.FieldGroup, error) {
-	const op = "FieldsRepository.GetFieldGroups"
-
-	var fg []domain.FieldGroup
-	err := filepath.Walk(s.jsonPath, func(path string, info os.FileInfo, err error) error {
-		if strings.Contains(info.Name(), "json") {
-
-			file, err := files.ReadJson(path)
-			if err != nil {
-				return &errors.Error{Code: errors.INTERNAL, Message: fmt.Sprintf("Unable to read field path with the path: %s", path), Operation: op, Err: err}
-			}
-
-			var fields domain.FieldGroup
-			err = json.Unmarshal(file, &fields)
-			if err != nil {
-				fmt.Println(err)
-				return &errors.Error{Code: errors.INTERNAL, Message: "Unable to unmarshal the field struct", Operation: op, Err: err}
-			}
-
-			fg = append(fg, fields)
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		return &[]domain.FieldGroup{}, err
-	}
-
-	return &fg, nil
-}
-
-// checkLocation checks to see if there has been a match within
-// the location passed, the string could be resource. page
-// template or anything defined within fields.
-func (s *FieldsStore) checkLocation(check string, location domain.FieldLocation) bool {
-	var match = false
-
-	switch location.Operator {
-	case "==":
-		{
-			if check == location.Value {
-				match = true
-			}
-		}
-	case "!=":
-		{
-			if check != location.Value {
-				match = true
-			}
-		}
-	}
-
-	return match
-}
-
-// checkMatch checks to see if the there has been a match within
-// the location block by using an array of booleans, if there
-// has already been a match, it will return false. Useful
-// for and location blocks not or.
-func (s *FieldsStore) checkMatch(matches []bool) bool {
-	for _, a := range matches {
-		if !a {
-			return false
-		}
-	}
-	return true
-}
-
-// hasBeenAdded checks to see if there already been a match within
-// the array of field groups.
-func (s *FieldsStore) hasBeenAdded(key string, fg []domain.FieldGroup) bool {
-	for _, v := range fg {
-		if v.UUID.String() == key {
-			return true
-		}
-	}
-	return false
+func (s *FieldsStore) GetLayout(p domain.Post, a domain.User, c *domain.Category) []domain.FieldGroup {
+	return fields.NewLocation().GetLayout(p, a, c, s.options.CacheServerFields)
 }
