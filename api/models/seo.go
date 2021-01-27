25d9f73f15d83ec876af0c87ec29c8935f5f274d
Ainsley Clark <info@ainsleyclark.com>
2021-01-27 09:39:57 +0000

    Bug fixes for media file paths and formatting post categories
    Former-commit-id: 40163fd5e8d550a4aacdfcddc5aa4c2a55bf5fee [formerly ccc15fdccafd07d964529bfc39c70302ab973f97]
    Former-commit-id: 4b9a448d9b3dc8c7de68f2494639063260a40dc1

--- api/models/seo.go
+++ api/models/seo.go
@@ -3,14 +3,13 @@ package models
 import (
 	"fmt"
 	"github.com/ainsleyclark/verbis/api/domain"
-
 	"github.com/ainsleyclark/verbis/api/errors"
 	"github.com/jmoiron/sqlx"
 )
 
 // SeoMetaRepository defines methods for Posts to interact with the database
 type SeoMetaRepository interface {
-	UpdateCreate(p *domain.PostData) error
+	UpdateCreate(id int, p domain.PostOptions) error
 }
 
 // SeoMetaStore defines the data layer for Seo & Meta Options
@@ -27,13 +26,13 @@ func newSeoMeta(db *sqlx.DB) *SeoMetaStore {
 
 // UpdateCreate checks to see if the record exists before updating
 // or creating the new record.
-func (s *SeoMetaStore) UpdateCreate(p *domain.PostData) error {
-	if s.exists(p.Id) {
-		if err := s.update(p); err != nil {
+func (s *SeoMetaStore) UpdateCreate(id int, p domain.PostOptions) error {
+	if s.exists(id) {
+		if err := s.update(id, p); err != nil {
 			return err
 		}
 	} else {
-		if err := s.create(p); err != nil {
+		if err := s.create(id, p); err != nil {
 			return err
 		}
 	}
@@ -49,22 +48,22 @@ func (s *SeoMetaStore) exists(id int) bool {
 
 // create a new seo meta record
 // Returns errors.INTERNAL if the SQL query was invalid.
-func (s *SeoMetaStore) create(p *domain.PostData) error {
+func (s *SeoMetaStore) create(id int, p domain.PostOptions) error {
 	const op = "SeoMetaRepository.create"
-	_, err := s.db.Exec("INSERT INTO post_options (post_id, seo, meta) VALUES (?, ?, ?)", p.Id, p.SeoMeta.Seo, p.SeoMeta.Meta)
+	_, err := s.db.Exec("INSERT INTO post_options (post_id, seo, meta) VALUES (?, ?, ?)", id, p.Seo, p.Meta)
 	if err != nil {
-		return &errors.Error{Code: errors.INTERNAL, Message: fmt.Sprintf("Could not create the seo meta options record for post title: %v", p.Title), Operation: op, Err: err}
+		return &errors.Error{Code: errors.INTERNAL, Message: fmt.Sprintf("Could not create the seo meta options record with the post id: %d", id), Operation: op, Err: err}
 	}
 	return nil
 }
 
 // update a seo meta record by page Id
 // Returns errors.INTERNAL if the SQL query was invalid.
-func (s *SeoMetaStore) update(p *domain.PostData) error {
+func (s *SeoMetaStore) update(id int, p domain.PostOptions) error {
 	const op = "SeoMetaRepository.update"
-	_, err := s.db.Exec("UPDATE post_options SET seo = ?, meta = ? WHERE post_id = ?", p.SeoMeta.Seo, p.SeoMeta.Meta, p.Id)
+	_, err := s.db.Exec("UPDATE post_options SET seo = ?, meta = ? WHERE post_id = ?", p.Seo, p.Meta, id)
 	if err != nil {
-		return &errors.Error{Code: errors.INTERNAL, Message: fmt.Sprintf("Could not update the seo meta options for the post title: %v", p.Title), Operation: op, Err: err}
+		return &errors.Error{Code: errors.INTERNAL, Message: fmt.Sprintf("Could not update the seo meta options with the post id: %d", id), Operation: op, Err: err}
 	}
 	return nil
 }
