58c07230ace8a4ccc78eceb9103cd081225b36af
Ainsley Clark <info@ainsleyclark.com>
2021-02-16 19:03:26 +0000

    Template engine adjustments
    Former-commit-id: 66dd9a16a029011e5f9fe2edf16df826a81d41c1 [formerly 564e2fa20d5b9da090c4aaced05ea66182d245a2]
    Former-commit-id: d30211cd8a67b436ca1d873cc62bfc8d743aa62d

--- api/tpl/funcs/paths/paths.go
+++ api/tpl/funcs/paths/paths.go
@@ -4,6 +4,70 @@
 
 package paths
 
+// Base
+//
+// Returns the base path of the project.
+//
+// Example: {{ basePath }}
+func (ns *Namespace) Base() string {
+	return ns.deps.Paths.Base
+}
+
+// Ad,om
+//
+// Returns the admin path of the project.
+//
+// Example: {{ adminPath }}
+func (ns *Namespace) Admin() string {
+	return ns.deps.Paths.Admin
+}
+
+// API
+//
+// Returns the API path of the project.
+//
+// Example: {{ apiPath }}
+func (ns *Namespace) API() string {
+	return ns.deps.Paths.API
+}
+
+// Theme
+//
+// Returns the Theme path of the project.
+//
+// Example: {{ themePath }}
+func (ns *Namespace) Theme() string {
+	return ns.deps.Paths.Theme
+}
+
+// Uploads
+//
+// Returns the Uploads path of the project.
+//
+// Example: {{ uploadsPath }}
+func (ns *Namespace) Uploads() string {
+	return ns.deps.Paths.Uploads
+}
+
+// Storage
+//
+// Returns the Storage path of the project.
+//
+// Example: {{ storagePath }}
+func (ns *Namespace) Storage() string {
+	return ns.deps.Paths.Storage
+}
+
+
+// Assets
+//
+// Returns the assets path of the theme.
+//
+// Example: {{ uploadsPath }}
+func (ns *Namespace) Assets() string {
+	return ns.deps.Theme.AssetsPath
+}
+
 // Templates
 //
 // Returns the directory where page templates
