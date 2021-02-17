752b8e861f00a83d3a6eb790155f399c1a4cdef5
Ainsley Clark <info@ainsleyclark.com>
2021-02-17 18:08:54 +0000

    Params testing
    Former-commit-id: 18f17a8573ceba77f0bb4d8b90f1a426c9a52fd1 [formerly 2f3444d08c0cc9c127e17969708f1b052ce58d4d]
    Former-commit-id: c08c8e46a80f50934f6391d5b9d42c89fd7e81b2

--- api/helpers/params/api.go
+++ api/helpers/params/api.go
@@ -6,6 +6,9 @@ package params
 
 import "github.com/gin-gonic/gin"
 
+// ApiParams
+//
+// Sets up a new Params struct with context.
 func ApiParams(g *gin.Context, def Defaults) *Params {
 	p := &Params{
 		Stringer: &apiParams{ctx: g},
@@ -14,10 +17,15 @@ func ApiParams(g *gin.Context, def Defaults) *Params {
 	return p
 }
 
+// apiParams defines the helper for returning context parameters.
 type apiParams struct {
 	ctx *gin.Context
 }
 
+// Param
+//
+// Satisfies the Stringer interface by returning a query
+// parameters to pass information to models.
 func (a *apiParams) Param(q string) string {
 	return a.ctx.Query(q)
 }
