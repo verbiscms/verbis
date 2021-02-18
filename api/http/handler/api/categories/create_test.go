da6dbd9833e058977e27782a301ae8f40dafb0af
Ainsley Clark <info@ainsleyclark.com>
2021-02-18 17:08:49 +0000

    Cleaning up http package
    Former-commit-id: 501a38b02ca347711068d887acd2f38b0ae98fa5 [formerly eaf479f5c3ac1df7019013a183d4d8dc4dd97542]
    Former-commit-id: 592421dbba3d8cf2cae06b20b90c593a592c61ef

--- api/http/handler/api/categories/create_test.go
+++ api/http/handler/api/categories/create_test.go
@@ -3,3 +3,94 @@
 // license that can be found in the LICENSE file.
 
 package categories
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/ainsleyclark/verbis/api/domain"
+	"github.com/ainsleyclark/verbis/api/errors"
+	mocks "github.com/ainsleyclark/verbis/api/mocks/models"
+	suite "github.com/ainsleyclark/verbis/api/test"
+	"github.com/gin-gonic/gin"
+	"testing"
+)
+
+func TestCategories_Create(t *testing.T) {
+
+	category := domain.Category{Id: 123, Slug: "/cat", Name: "Category", Resource: "test"}
+	categoryBadValidation := domain.Category{Id: 123, Name: "Category", Resource: "test"}
+
+	tt := map[string]struct {
+		want    *domain.Category
+		status  int
+		message string
+		input   interface{}
+		mock    func(m *mocks.CategoryRepository)
+	}{
+		"Success": {
+			want:    &category,
+			status:  200,
+			message: "Successfully created category with ID: 123",
+			input:   category,
+			mock: func(m *mocks.CategoryRepository) {
+				m.On("Create", &category).Return(category, nil)
+			},
+		},
+		"Validation Failed": {
+			want:    `{"errors":[{"key":"slug","message":"Slug is required.","type":"required"}]}`,
+			status:  400,
+			message: "Validation failed",
+			input:   categoryBadValidation,
+			mock: func(m *mocks.CategoryRepository) {
+				m.On("Create", &categoryBadValidation).Return(domain.Category{}, fmt.Errorf("error"))
+			},
+		},
+		"Invalid": {
+			want:    `{}`,
+			status:  400,
+			message: "invalid",
+			input:   category,
+			mock: func(m *mocks.CategoryRepository) {
+				m.On("Create", &category).Return(domain.Category{}, &errors.Error{Code: errors.INVALID, Message: "invalid"})
+			},
+		},
+		"Conflict": {
+			want:    `{}`,
+			status:  400,
+			message: "conflict",
+			input:   category,
+			mock: func(m *mocks.CategoryRepository) {
+				m.On("Create", &category).Return(domain.Category{}, &errors.Error{Code: errors.CONFLICT, Message: "conflict"})
+			},
+		},
+		"Internal Error": {
+			want:    `{}`,
+			status:  500,
+			message: "internal",
+			input:   category,
+			mock: func(m *mocks.CategoryRepository) {
+				m.On("Create", &category).Return(domain.Category{}, &errors.Error{Code: errors.INTERNAL, Message: "internal"})
+			},
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func(t *testing.T) {
+			rr := suite.APITestSuite(t)
+			mock := &mocks.CategoryRepository{}
+			test.mock(mock)
+
+			body, err := json.Marshal(test.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rr.RequestAndServe("POST", "/categories", "/categories", bytes.NewBuffer(body), func(g *gin.Context) {
+				Mock(mock).Create(g)
+			})
+
+			rr.Run(&domain.Category{}, test.want, test.status, test.message)
+		})
+	}
+}
--- api/http/handler/api/categories/list_test.go
+++ api/http/handler/api/categories/list_test.go
@@ -3,3 +3,77 @@
 // license that can be found in the LICENSE file.
 
 package categories
+
+import (
+	"fmt"
+	"github.com/ainsleyclark/verbis/api/domain"
+	"github.com/ainsleyclark/verbis/api/errors"
+	mocks "github.com/ainsleyclark/verbis/api/mocks/models"
+	suite "github.com/ainsleyclark/verbis/api/test"
+	"github.com/gin-gonic/gin"
+	"testing"
+)
+
+func TestRedirects_Find(t *testing.T) {
+
+	category := domain.Category{Id: 123, Slug: "/cat", Name: "Category"}
+
+	tt := map[string]struct {
+		want    *domain.Category
+		status  int
+		message string
+		mock    func(m *mocks.CategoryRepository)
+		url     string
+	}{
+		"Success": {
+			want:   &category,
+			status:  200,
+			message: "Successfully obtained category with ID: 123",
+			mock: func(m *mocks.CategoryRepository) {
+				m.On("GetById", 123).Return(category, nil)
+			},
+			url: "/categories/123",
+		},
+		"Invalid ID": {
+			want:    nil,
+			status:  400,
+			message: "Pass a valid number to obtain the category by ID",
+			mock: func(m *mocks.CategoryRepository) {
+				m.On("GetById", 123).Return(domain.Category{}, fmt.Errorf("error"))
+			},
+			url: "/categories/wrongid",
+		},
+		"Not Found": {
+			want:    nil,
+			status:  200,
+			message: "no categories found",
+			mock: func(m *mocks.CategoryRepository) {
+				m.On("GetById", 123).Return(domain.Category{}, &errors.Error{Code: errors.NOTFOUND, Message: "no categories found"})
+			},
+			url: "/categories/123",
+		},
+		"Internal Error": {
+			want:    nil,
+			status:  500,
+			message: "internal",
+			mock: func(m *mocks.CategoryRepository) {
+				m.On("GetById", 123).Return(domain.Category{}, &errors.Error{Code: errors.INTERNAL, Message: "internal"})
+			},
+			url: "/categories/123",
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func(t *testing.T) {
+			rr := suite.APITestSuite(t)
+			mock := &mocks.CategoryRepository{}
+			test.mock(mock)
+
+			rr.RequestAndServe("GET", test.url, "/categories/:id", nil, func(g *gin.Context) {
+				Mock(mock).Find(g)
+			})
+
+			rr.Run(&domain.Category{}, test.want, test.status, test.message)
+		})
+	}
+}
