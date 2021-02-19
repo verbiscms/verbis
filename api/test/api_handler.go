cc26e5b60be5f1d3a70575b38c5265b62694ff09
Ainsley Clark <ainsley@reddico.co.uk>
2021-02-19 07:31:10 +0000

    HTTP Testing
    Former-commit-id: d6f2652aa1cd93790111957cafebdd379bbe04ae [formerly 63f4167015b24d64beeed6cd93f9b19451f8a73d]
    Former-commit-id: 98f3a06dc602987c02d908b2a019f22b1d90203e

--- api/test/api_handler.go
+++ api/test/api_handler.go
@@ -8,139 +8,124 @@ import (
 	"encoding/json"
 	"github.com/ainsleyclark/verbis/api/http/handler/api"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/suite"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"testing"
 )
 
 const (
 	JsonHeader = "application/json; charset=utf-8"
 )
 
-// controllerTest represents the suite of testing methods for controllers.
-type controllerTest struct {
-	testing  *testing.T
-	recorder *httptest.ResponseRecorder
-	ctx      *gin.Context
-	engine   *gin.Engine
-	respond  api.RespondJson
-	got interface{}
+// HandlerSuite represents the suite of testing methods for controllers.
+type HandlerSuite struct {
+	suite.Suite
+	Recorder *httptest.ResponseRecorder
+	Context  *gin.Context
+	Engine   *gin.Engine
 }
+
 
 // APITestSuite - New recorder for testing
 // controllers, initialises gin & sets gin mode.
-func APITestSuite(t *testing.T) *controllerTest {
+func APITestSuite() HandlerSuite {
 	gin.SetMode(gin.TestMode)
 	gin.DefaultWriter = ioutil.Discard
 	rr := httptest.NewRecorder()
 	ctx, engine := gin.CreateTestContext(rr)
 
-	return &controllerTest{
-		testing:  t,
-		recorder: rr,
-		ctx:      ctx,
-		engine:   engine,
+	return HandlerSuite{
+		Recorder: rr,
+		Context:      ctx,
+		Engine:   engine,
 	}
 }
 
-// ToMap converts a struct to a map using the struct's tags.
-//func (c *controllerTest) DataBytes(in interface{}) map[string]interface{} {
-//	out, err := json.Marshal(in)
-//	if err != nil {
-//		c.testing.Error(err)
-//	}
-//
-//
-//
-//
-//	fmt.Print(string(out))
-//	var m = map[string]interface{}{}
-//	err = json.Unmarshal(out, &m)
-//	if err != nil {
-//		c.testing.Error(err)
-//	}
-//
-//	return m
-//}
+// Run the API test.
+func (t *HandlerSuite) RunT(want interface{}, status int, message string) {
+	defer func() {
+		t.Reset()
+	}()
 
-func (c *controllerTest) TestIn(i interface{}) string {
+	got, data := t.decode()
+	t.Equal(message, got.Message)
+	t.Equal(status, t.Status())
+	t.Equal(JsonHeader, t.ContentType())
+	t.JSONEq(t.marshalInput(want), data)
+}
+
+func (t *HandlerSuite) Status() int {
+	return t.Recorder.Code
+}
+
+func (t *HandlerSuite) ContentType() string {
+	return t.Recorder.Header().Get("Content-Type")
+}
+
+// NewRequest makes a new http.Request and assigns the gin testing
+// the request.
+func (t *HandlerSuite) NewRequest(method string, url string, body io.Reader) {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		t.Fail("error creating http request", err)
+	}
+	t.Context.Request = req
+}
+
+// RequestAndServe makes a new http.Request and assigns the gin testing
+// the request, serves HTTP.
+func (t *HandlerSuite) RequestAndServe(method string, url string, engineUrl string, body io.Reader, handler func(ctx *gin.Context)) {
+	switch method {
+	case "GET":
+		t.Engine.GET(engineUrl, handler)
+	case "POST":
+		t.Engine.POST(engineUrl, handler)
+	case "PUT":
+		t.Engine.PUT(engineUrl, handler)
+	case "DELETE":
+		t.Engine.DELETE(engineUrl, handler)
+	}
+	t.NewRequest(method, url, body)
+	t.Engine.ServeHTTP(t.Recorder, t.Context.Request)
+}
+
+func (t *HandlerSuite) Reset() {
+	rr := httptest.NewRecorder()
+	ctx, engine := gin.CreateTestContext(rr)
+	t.Recorder = rr
+	t.Context = ctx
+	t.Engine = engine
+}
+
+func (t *HandlerSuite) marshalInput(i interface{}) string {
 	str, ok := i.(string)
 	if ok {
 		return str
 	}
+	if i == nil {
+		return "{}"
+	}
 	out, err := json.Marshal(i)
 	if err != nil {
-		c.testing.Error(err)
+		t.Fail("error marshalling input", err)
 	}
 	return string(out)
 }
 
 // Run the API test.
-func (c *controllerTest) TestRun() (api.RespondJson, string) {
+func (t *HandlerSuite) decode() (api.RespondJson, string) {
 	responder := &api.RespondJson{}
-	err := json.NewDecoder(c.recorder.Body).Decode(responder)
+	err := json.NewDecoder(t.Recorder.Body).Decode(responder)
 	if err != nil {
-		c.testing.Error(err)
+		t.Fail("error decoding body", err)
 	}
-	c.respond = *responder
 
 	out, err := json.Marshal(responder.Data)
 	if err != nil {
-		c.testing.Error(err)
+		t.Fail("error marshalling data", err)
 	}
-	c.respond.Data = string(out)
 
-	return c.respond, string(out)
-}
-
-func (c *controllerTest) Status() int {
-	return c.recorder.Code
-}
-
-func (c *controllerTest) ContentType() string {
-	return c.recorder.Header().Get("Content-Type")
-}
-
-// Run the API test.
-func (c *controllerTest) Run(typ interface{}, want interface{}, status int, message string) {
-	//c.unmarshal(typ)
-	//assert.Equal(c.testing, status, c.Recorder.Code)
-	//assert.Equal(c.testing, message, c.respond.Message)
-	//assert.Equal(c.testing, c.Recorder.Header().Get("Content-Type"), "application/json; charset=utf-8")
-	//
-	//if reflect.ValueOf(want).IsZero() {
-	//	assert.Equal(c.testing, typ, c.got)
-	//	return
-	//}
-	//
-	//assert.Equal(c.testing, want, c.got)
-}
-
-// NewRequest makes a new http.Request and assigns the gin testing
-// the request.
-func (c *controllerTest) NewRequest(method string, url string, body io.Reader) {
-	req, err := http.NewRequest(method, url, body)
-	if err != nil {
-		c.testing.Fatal(err)
-	}
-	c.ctx.Request = req
-}
-
-// RequestAndServe makes a new http.Request and assigns the gin testing
-// the request, serves HTTP.
-func (c *controllerTest) RequestAndServe(method string, url string, engineUrl string, body io.Reader, handler func(ctx *gin.Context)) {
-	switch method {
-	case "GET":
-		c.engine.GET(engineUrl, handler)
-	case "POST":
-		c.engine.POST(engineUrl, handler)
-	case "PUT":
-		c.engine.PUT(engineUrl, handler)
-	case "DELETE":
-		c.engine.DELETE(engineUrl, handler)
-	}
-	c.NewRequest(method, url, body)
-	c.engine.ServeHTTP(c.recorder, c.ctx.Request)
+	return *responder, string(out)
 }
