bebe40c0b6dfeb00c721a778b051bc266bd629cc
Ainsley Clark <info@ainsleyclark.com>
2021-02-19 12:45:29 +0000

    HTTP Testing
    Former-commit-id: 529fa2cd19394f07feb7d84a904492695d6a56a9 [formerly 37ff6bb68c339cdb467e11652bc360e6bb8626ec]
    Former-commit-id: 8f6c306de9d0b04e665a10db86df3fc04a8c2818

--- api/http/handler/api/posts/update_test.go
+++ api/http/handler/api/posts/update_test.go
@@ -3,3 +3,86 @@
 // license that can be found in the LICENSE file.
 
 package posts
+
+import (
+	"fmt"
+	"github.com/ainsleyclark/verbis/api/domain"
+	"github.com/ainsleyclark/verbis/api/errors"
+	validation "github.com/ainsleyclark/verbis/api/helpers/vaidation"
+	"github.com/ainsleyclark/verbis/api/http/handler/api"
+	mocks "github.com/ainsleyclark/verbis/api/mocks/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (t *PostsTestSuite) TestPosts_Update() {
+
+	tt := map[string]struct {
+		want    interface{}
+		status  int
+		message string
+		input   interface{}
+		mock    func(m *mocks.PostsRepository)
+		url     string
+	}{
+		"Success": {
+			postData,
+			200,
+			"Successfully updated post with ID: 123",
+			post,
+			func(m *mocks.PostsRepository) {
+				m.On("Update", &postCreate).Return(postData, nil)
+			},
+			"/posts/123",
+		},
+		"Validation Failed": {
+			api.ValidationErrJson{Errors: validation.Errors{{Key: "slug", Message: "Post Slug is required.", Type: "required"}}},
+			400,
+			"Validation failed",
+			postBadValidation,
+			func(m *mocks.PostsRepository) {
+				m.On("Update", postBadValidation).Return(domain.PostData{}, fmt.Errorf("error"))
+			},
+			"/posts/123",
+		},
+		"Invalid ID": {
+			nil,
+			400,
+			"A valid ID is required to update the post",
+			post,
+			func(m *mocks.PostsRepository) {
+				m.On("Update", &postCreate).Return(domain.PostData{}, fmt.Errorf("error"))
+			},
+			"/posts/wrongid",
+		},
+		"Not Found": {
+			nil,
+			400,
+			"not found",
+			post,
+			func(m *mocks.PostsRepository) {
+				m.On("Update", &postCreate).Return(domain.PostData{}, &errors.Error{Code: errors.NOTFOUND, Message: "not found"})
+			},
+			"/posts/123",
+		},
+		"Internal": {
+			nil,
+			500,
+			"internal",
+			post,
+			func(m *mocks.PostsRepository) {
+				m.On("Update", &postCreate).Return(domain.PostData{}, &errors.Error{Code: errors.INTERNAL, Message: "internal"})
+			},
+			"/posts/123",
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func() {
+			t.RequestAndServe(http.MethodPut, test.url, "/posts/:id", test.input, func(ctx *gin.Context) {
+				t.Setup(test.mock).Update(ctx)
+			})
+			t.RunT(test.want, test.status, test.message)
+		})
+	}
+}
--- api/http/handler/api/site/layouts_test.go
+++ api/http/handler/api/site/layouts_test.go
@@ -3,3 +3,55 @@
 // license that can be found in the LICENSE file.
 
 package site
+
+import (
+	"github.com/ainsleyclark/verbis/api/domain"
+	"github.com/ainsleyclark/verbis/api/errors"
+	mocks "github.com/ainsleyclark/verbis/api/mocks/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (t *SiteTestSuite) TestSite_Layouts() {
+	
+	tt := map[string]struct {
+		want    interface{}
+		status  int
+		message string
+		mock    func(m *mocks.SiteRepository)
+	}{
+		"Success": {
+			layouts,
+			200,
+			"Successfully obtained layouts",
+			func(m *mocks.SiteRepository) {
+				m.On("GetLayouts").Return(layouts, nil)
+			},
+		},
+		"Not Found": {
+			`{}`,
+			200,
+			"not found",
+			func(m *mocks.SiteRepository) {
+				m.On("GetLayouts").Return(domain.Layouts{}, &errors.Error{Code: errors.NOTFOUND, Message: "not found"})
+			},
+		},
+		"Internal Error": {
+			`{}`,
+			500,
+			"internal",
+			func(m *mocks.SiteRepository) {
+				m.On("GetLayouts").Return(domain.Layouts{}, &errors.Error{Code: errors.INTERNAL, Message: "internal"})
+			},
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func() {
+			t.RequestAndServe(http.MethodGet, "/theme", "/theme", nil, func(ctx *gin.Context) {
+				t.Setup(test.mock).Layouts(ctx)
+			})
+			t.RunT(test.want, test.status, test.message)
+		})
+	}
+}
