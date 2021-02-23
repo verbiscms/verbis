f121f23df6c7bf33b5146d09fefea47bf16f54d7
Ainsley Clark <info@ainsleyclark.com>
2021-02-23 16:12:43 +0000

    Cleaned up env
    Former-commit-id: 1e53968ff31fec640e7199aca8f889f52bb498d8 [formerly 4b9d1662826dd2efcf64ae9235d6e5b14cfbb58a]
    Former-commit-id: 033637b002c87a2ec61bb56cf5ae2bf2e49a5041

--- api/environment/env.go
+++ api/environment/env.go
@@ -16,27 +16,32 @@ import (
 	"path/filepath"
 )
 
-// |||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// Env defines the environment variables set in the .env file.
 type Env struct {
+	// Prod, production or dev.
 	AppEnv          string `json:"APP_ENV"`
+	// If Verbis is in debug mode (true, false).
 	AppDebug        string `json:"APP_DEBUG"`
+	// The port the server should listen to.
 	AppPort         string `json:"APP_PORT" binding:"required"`
+	// The database host (IP) for the store.
 	DbHost          string `json:"DB_HOST" binding:"required"`
+	// The database port for the store.
 	DbPort          string `json:"DB_PORT" binding:"required"`
+	// The database name.
 	DbDatabase      string `json:"DB_DATABASE" binding:"required"`
+	// The database user name.
 	DbUser          string `json:"DB_USERNAME" binding:"required"`
+	// The database port.
 	DbPassword      string `json:"DB_PASSWORD" binding:"required"`
+	// The key for Sparkpost (mailer).
 	SparkpostApiKey string `json:"SPARKPOST_API_KEY"`
+	// The url for Sparkpost (could be EU).
 	SparkpostUrl    string `json:"SPARKPOST_URL"`
+	// The mailing from address.
 	MailFromAddress string `json:"MAIL_FROM_ADDRESS"`
+	// The mailing from name.
 	MailFromName    string `json:"MAIL_FROM_NAME"`
-}
-
-type Mail struct {
-	SparkpostApiKey string `json:"SPARKPOST_API_KEY"`
-	SparkpostUrl    string `json:"SPARKPOST_URL"`
-	FromAddress     string `json:"MAIL_FROM_ADDRESS"`
-	FromName        string `json:"MAIL_FROM_NAME"`
 }
 
 var (
@@ -49,7 +54,9 @@ var (
 
 // Load
 //
-// Load populates environment, loads and validates the environment file.
+// Load populates environment, loads and validates the
+// environment file.
+//
 // Returns errors.INVALID if the env file failed to load.
 func Load() (*Env, error) {
 	const op = "environment.Load"
@@ -77,7 +84,9 @@ func Load() (*Env, error) {
 
 // Validate
 //
-// the environment file for missing keys
+// Validates the environment file for missing keys, if
+// there are no validation errors, nil will be
+// returned.
 func (e *Env) Validate() validation.Errors {
 	v := validation.New()
 	err := v.Package.Struct(e)
@@ -90,8 +99,9 @@ func (e *Env) Validate() validation.Errors {
 
 // Port
 //
-// |||||||||||||||||||||||||||||||||||||||||||||||||||||||
-//
+// Returns the env port as an integer, if the strconv
+// produced an error, the default port of 5000 will
+// be used,
 func (e *Env) Port() int {
 	const op = "Env.Port"
 	port, err := strconv.Atoi(e.AppPort)
@@ -104,16 +114,22 @@ func (e *Env) Port() int {
 
 // ConnectString
 //
-// |||||||||||||||||||||||||||||||||||||||||||||||||||||||
-//
+// Returns the MySQL database connection string.
 func (e *Env) ConnectString() string {
 	return e.DbUser + ":" + e.DbPassword + "@tcp(" + e.DbHost + ":" + e.DbPort + ")/" + e.DbDatabase + "?tls=false&parseTime=true&multiStatements=true"
 }
 
+// Mail defines the configuration for sending emails.
+type Mail struct {
+	SparkpostApiKey string `json:"SPARKPOST_API_KEY"`
+	SparkpostUrl    string `json:"SPARKPOST_URL"`
+	FromAddress     string `json:"MAIL_FROM_ADDRESS"`
+	FromName        string `json:"MAIL_FROM_NAME"`
+}
+
 // MailConfig
 //
-// |||||||||||||||||||||||||||||||||||||||||||||||||||||||
-//
+// Returns the configuration for the mailer.
 func (e *Env) MailConfig() Mail {
 	return Mail{
 		FromAddress:     e.MailFromAddress,
@@ -125,16 +141,14 @@ func (e *Env) MailConfig() Mail {
 
 // IsProduction
 //
-// |||||||||||||||||||||||||||||||||||||||||||||||||||||||
-//
+// If the application is set to production.
 func (e *Env) IsProduction() bool {
 	return e.AppEnv == "production" || e.AppEnv == "prod"
 }
 
 // IsDebug
 //
-// |||||||||||||||||||||||||||||||||||||||||||||||||||||||
-//
+// If the application is set to debug.
 func (e *Env) IsDebug() bool {
 	return e.AppDebug != "false"
 }
