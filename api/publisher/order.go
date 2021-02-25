df6a6c4bcbf9502a901e3e1aefc8c77df2f32ab8
Ainsley Clark <info@ainsleyclark.com>
2021-02-25 12:58:31 +0000

    Cleaned up theme configurationlazygit

---
+++ api/publisher/order.go
@@ -0,0 +1,18 @@
+// Copyright 2020 The Verbis Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package publisher
+
+// Check if there is a resource
+//  Check if there is  a category
+// If there
+
+type TypeOfPage struct {
+
+
+}
+
+func (r *Render) resolve() {
+
+}
