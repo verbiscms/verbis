0268dd5fa3df0665b8b3a894e6c3bbecc4306cb9
Ainsley Clark <info@ainsleyclark.com>
2021-03-09 09:01:18 +0000

    Category store refactor
    Former-commit-id: 487719733f5bf133cc1aae31994341d428ceddb7 [formerly d0bb350e8175eae3b782f8f39c746568b01ec92c]
    Former-commit-id: a0f4623053daf140689e6f136d35b618078f2440

--- api/models/categories/categories.go
+++ api/models/categories/categories.go
@@ -15,16 +15,18 @@ import (
 // Repository defines methods for categories
 // to interact with the database.
 type Repository interface {
-	Get(meta params.Params) (domain.Categories, int, error)
-	Find(id int64) (domain.Category, error)
-	FindByPost(pageID int) (*domain.Category, error)
+	List(meta params.Params) (domain.Categories, int, error)
+	Find(id int) (domain.Category, error)
+	FindByPost(id int) (domain.Category, error)
 	FindBySlug(slug string) (domain.Category, error)
 	FindByName(name string) (domain.Category, error)
-	GetParent(id int) (domain.Category, error)
-	Create(c *domain.Category) (domain.Category, error)
-	Update(c *domain.Category) (domain.Category, error)
+	//GetParent(id int) (domain.Category, error)
+	Create(c domain.Category) (domain.Category, error)
+	Update(c domain.Category) (domain.Category, error)
 	Delete(id int) error
 	Exists(id int) bool
+	ExistsByName(name string) bool
+	ExistsBySlug(slug string) bool
 }
 
 // Store defines the data layer for Categories.
@@ -33,14 +35,20 @@ type Store struct {
 }
 
 var (
+	// Validation error when a category already exists.
 	ErrCategoryExists = errors.New("category already exists")
 )
 
 const (
-	TableName      = "categories"
+	// The database table name for categories.
+	TableName = "categories"
+	// The database table name for the categories pivot.
 	PivotTableName = "post_categories"
 )
 
+// New
+//
+// Creates a new Categories store.
 func New(db *sqlx.DB) *Store {
 	return &Store{
 		Model: database.NewModel(db),
--- api/models/categories/find_test.go
+++ api/models/categories/find_test.go
@@ -18,6 +18,7 @@ var (
 	FindByPostQuery = "SELECT `c`.* FROM `post_categories` LEFT JOIN `categories` AS `c` ON `post_categories`.`post_id` = `c`.`id` WHERE `post_categories`.`post_id` = '1'"
 	FindBySlugQuery = "SELECT * FROM `categories` WHERE `slug` = '" + category.Slug + "' LIMIT 1"
 	FindByNameQuery = "SELECT * FROM `categories` WHERE `name` = '" + category.Name + "' LIMIT 1"
+	FindParentQuery = "SELECT * FROM `categories` WHERE `parent_id` = '" + categoryID + "' LIMIT 1"
 )
 
 func (t *CategoriesTestSuite) TestStore_Find() {
@@ -171,6 +172,46 @@ func (t *CategoriesTestSuite) TestStore_FindByName() {
 		t.Run(name, func() {
 			s := t.Setup(test.mock)
 			got, err := s.FindByName(category.Name)
+			if err != nil {
+				t.Contains(errors.Message(err), test.want)
+				return
+			}
+			t.RunT(test.want, got)
+		})
+	}
+}
+
+func (t *CategoriesTestSuite) TestStore_FindParent() {
+	tt := map[string]struct {
+		want interface{}
+		mock func(m sqlmock.Sqlmock)
+	}{
+		"Success": {
+			category,
+			func(m sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"id", "slug", "name"}).
+					AddRow(category.Id, category.Slug, category.Name)
+				m.ExpectQuery(regexp.QuoteMeta(FindParentQuery)).WillReturnRows(rows)
+			},
+		},
+		"No Rows": {
+			"No category exists with the parent ID",
+			func(m sqlmock.Sqlmock) {
+				m.ExpectQuery(regexp.QuoteMeta(FindParentQuery)).WillReturnError(sql.ErrNoRows)
+			},
+		},
+		"Internal Error": {
+			database.ErrQueryMessage,
+			func(m sqlmock.Sqlmock) {
+				m.ExpectQuery(regexp.QuoteMeta(FindParentQuery)).WillReturnError(fmt.Errorf("error"))
+			},
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func() {
+			s := t.Setup(test.mock)
+			got, err := s.FindParent(category.Id)
 			if err != nil {
 				t.Contains(errors.Message(err), test.want)
 				return
