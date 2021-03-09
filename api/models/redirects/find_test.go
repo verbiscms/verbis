7a42e1edad904fc98d592d2c2e4401f0d6da4c7b
Ainsley Clark <info@ainsleyclark.com>
2021-03-09 11:06:36 +0000

    Redirects store

--- api/models/redirects/find_test.go
+++ api/models/redirects/find_test.go
@@ -14,7 +14,8 @@ import (
 )
 
 var (
-	FindQuery = "SELECT * FROM `redirects` WHERE `id` = '" + redirectID + "' LIMIT 1"
+	FindQuery       = "SELECT * FROM `redirects` WHERE `id` = '" + redirectID + "' LIMIT 1"
+	FindByFromQuery = "SELECT * FROM `redirects` WHERE `from_path` = '" + redirect.From + "' LIMIT 1"
 )
 
 func (t *RedirectsTestSuite) TestStore_Find() {
@@ -48,6 +49,46 @@ func (t *RedirectsTestSuite) TestStore_Find() {
 		t.Run(name, func() {
 			s := t.Setup(test.mock)
 			got, err := s.Find(redirect.Id)
+			if err != nil {
+				t.Contains(errors.Message(err), test.want)
+				return
+			}
+			t.RunT(test.want, got)
+		})
+	}
+}
+
+func (t *RedirectsTestSuite) TestStore_FindByFrom() {
+	tt := map[string]struct {
+		want interface{}
+		mock func(m sqlmock.Sqlmock)
+	}{
+		"Success": {
+			redirect,
+			func(m sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"id", "from_path", "to_path", "code"}).
+					AddRow(redirect.Id, redirect.From, redirect.To, redirect.Code)
+				m.ExpectQuery(regexp.QuoteMeta(FindByFromQuery)).WillReturnRows(rows)
+			},
+		},
+		"No Rows": {
+			"No redirect exists with the from path",
+			func(m sqlmock.Sqlmock) {
+				m.ExpectQuery(regexp.QuoteMeta(FindByFromQuery)).WillReturnError(sql.ErrNoRows)
+			},
+		},
+		"Internal": {
+			database.ErrQueryMessage,
+			func(m sqlmock.Sqlmock) {
+				m.ExpectQuery(regexp.QuoteMeta(FindByFromQuery)).WillReturnError(fmt.Errorf("error"))
+			},
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func() {
+			s := t.Setup(test.mock)
+			got, err := s.FindByFrom(redirect.From)
 			if err != nil {
 				t.Contains(errors.Message(err), test.want)
 				return
