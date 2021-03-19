064cf01375ba6c01bb1e375d433710ae12f78167
Ainsley Clark <info@ainsleyclark.com>
2021-03-19 12:27:10 +0000

    Finished media
    Former-commit-id: 6da6ff91c8c14b738878fe7cc69a188c7d90ea6d [formerly 4f7453ea3b99d789579832dd66624980a9242517]
    Former-commit-id: 06076415b3d0164502a4e9df7c1da3a6fdb02cd8

---
+++ api/services/media/serve_test.go
@@ -0,0 +1,94 @@
+// Copyright 2020 The Verbis Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package media
+
+import (
+	"github.com/ainsleyclark/verbis/api/domain"
+	"github.com/ainsleyclark/verbis/api/errors"
+	"github.com/google/uuid"
+)
+
+func (t *MediaTestSuite) TestClient_Serve() {
+	tt := map[string]struct {
+		webp    bool
+		input   domain.Media
+		options domain.Options
+		want    interface{}
+	}{
+		"PNG": {
+			false,
+			domain.Media{
+				UUID:     uuid.MustParse("a66e03d9-cc64-4176-b226-47d0e7a2debb"),
+				FileSize: 0,
+				Url:      "/test.png",
+				FileName: "test.png",
+				Mime:     "image/png",
+			},
+			domain.Options{},
+			domain.Mime("image/png"),
+		},
+		"JPG": {
+			false,
+			domain.Media{
+				UUID:     uuid.MustParse("a66e03d9-cc64-4176-b226-47d0e7a2debb"),
+				FileSize: 0,
+				Url:      "/test.jpg",
+				FileName: "test.jpg",
+				Mime:     "image/jpg",
+			},
+			domain.Options{},
+			domain.Mime("image/jpg"),
+		},
+		"WebP": {
+			true,
+			domain.Media{
+				UUID:     uuid.MustParse("a66e03d9-cc64-4176-b226-47d0e7a2debb"),
+				FileSize: 0,
+				Url:      "/test.jpg",
+				FileName: "test.jpg",
+				Mime:     "image/jpg",
+			},
+			domain.Options{
+				MediaServeWebP: true,
+			},
+			domain.Mime("image/webp"),
+		},
+		"WebP Failed": {
+			true,
+			domain.Media{
+				UUID:     uuid.MustParse("2af11d5c-88ae-11eb-8dcd-0242ac130003"),
+				FileSize: 0,
+				Url:      "/test.jpg",
+				FileName: "test.jpg",
+				Mime:     "image/jpg",
+			},
+			domain.Options{
+				MediaServeWebP: true,
+			},
+			domain.Mime("image/jpg"),
+		},
+		"Not Found": {
+			false,
+			domain.Media{
+				UUID: uuid.UUID{},
+			},
+			domain.Options{},
+			"File does not exist with the path",
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func() {
+			s := t.Setup(domain.ThemeConfig{}, test.options)
+			_, mime, err := s.Serve(test.input, test.webp)
+			if err != nil {
+				t.Contains(errors.Message(err), test.want)
+				t.Equal(domain.Mime(""), mime)
+				return
+			}
+			t.Equal(test.want, mime)
+		})
+	}
+}
