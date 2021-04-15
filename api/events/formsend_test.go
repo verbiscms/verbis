fd5130d03aea060af7fd32d04ef8d1d0d5fd16e1
Ainsley Clark <ainsley@reddico.co.uk>
2021-04-15 12:13:00 +0100

    Testing mail package
    Former-commit-id: bf8c7245fa9ff576bf3fb2145f0367d408fd26d5
    Former-commit-id: bbb1c90d57437ecae6637cae7f9b7e5df3ff245e

--- api/events/formsend_test.go
+++ api/events/formsend_test.go
@@ -4,68 +4,64 @@
 
 package events
 
-import (
-	"github.com/ainsleyclark/verbis/api/domain"
-	"github.com/ainsleyclark/verbis/api/errors"
-)
-
-func (t *EventTestSuite) Test_FormSendDispatch() {
-	tt := map[string]struct {
-		data  interface{}
-		error bool
-		want  interface{}
-	}{
-		"Success": {
-			FormSend{
-				Form:   &domain.Form{Id: 1},
-				Values: domain.FormValues{},
-			},
-			false,
-			nil,
-		},
-		"Send Error": {
-			FormSend{
-				Form:   &domain.Form{Id: 1},
-				Values: domain.FormValues{},
-			},
-			true,
-			errors.GlobalError,
-		},
-		"Nil Form": {
-			FormSend{
-				Form:   nil,
-				Values: domain.FormValues{},
-			},
-			true,
-			"Form cannot be nil",
-		},
-		//"With Values": {
-		//	FormSend{
-		//		Form:   &domain.Form{Id: 1},
-		//		Values: domain.FormValues{
-		//			"test": "test"
-		//		},
-		//	},
-		//	false,
-		//	nil,
-		//},
-		"Validation failed": {
-			event{},
-			true,
-			"FormSend should be passed to dispatch",
-		},
-	}
-
-	for name, test := range tt {
-		t.Run(name, func() {
-			deps := t.Setup(test.error)
-			dispatcher := NewFormSend(deps)
-			err := dispatcher.Dispatch(test.data, []string{"hello@verbiscms.com"}, nil)
-			if err != nil {
-				t.Contains(errors.Message(err), test.want)
-				return
-			}
-			t.Equal(test.want, err)
-		})
-	}
-}
+//
+//func (t *EventTestSuite) Test_FormSendDispatch() {
+//	tt := map[string]struct {
+//		data  interface{}
+//		error bool
+//		want  interface{}
+//	}{
+//		"Success": {
+//			FormSend{
+//				Form:   &domain.Form{Id: 1},
+//				Values: domain.FormValues{},
+//			},
+//			false,
+//			nil,
+//		},
+//		"Send Error": {
+//			FormSend{
+//				Form:   &domain.Form{Id: 1},
+//				Values: domain.FormValues{},
+//			},
+//			true,
+//			errors.GlobalError,
+//		},
+//		"Nil Form": {
+//			FormSend{
+//				Form:   nil,
+//				Values: domain.FormValues{},
+//			},
+//			true,
+//			"Form cannot be nil",
+//		},
+//		//"With Values": {
+//		//	FormSend{
+//		//		Form:   &domain.Form{Id: 1},
+//		//		Values: domain.FormValues{
+//		//			"test": "test"
+//		//		},
+//		//	},
+//		//	false,
+//		//	nil,
+//		//},
+//		"Validation failed": {
+//			event{},
+//			true,
+//			"FormSend should be passed to dispatch",
+//		},
+//	}
+//
+//	for name, test := range tt {
+//		t.Run(name, func() {
+//			deps := t.Setup(test.error)
+//			dispatcher := NewFormSend(deps)
+//			err := dispatcher.Dispatch(test.data, []string{"hello@verbiscms.com"}, nil)
+//			if err != nil {
+//				t.Contains(errors.Message(err), test.want)
+//				return
+//			}
+//			t.Equal(test.want, err)
+//		})
+//	}
+//}
