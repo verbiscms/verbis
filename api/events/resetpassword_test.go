733688116a46a330e9bc138828e31e0f9aa52ce4
Ainsley Clark <ainsley@reddico.co.uk>
2021-04-15 12:15:17 +0100

    Testing mail package
    Former-commit-id: f7159e61ed8f8e0ed7a604d86185af53df9e11f4
    Former-commit-id: 953559e02da3fa68f1f612bbd70ae34f77e17689

--- api/events/resetpassword_test.go
+++ api/events/resetpassword_test.go
@@ -4,45 +4,42 @@
 
 package events
 
-//func (t *EventTestSuite) Test_ResetPasswordDispatch() {
-//	tt := map[string]struct {
-//		data  interface{}
-//		error bool
-//		want  interface{}
-//	}{
-//		"Success": {
-//			ResetPassword{
-//				Token: "token",
-//				User:  domain.UserPart{},
-//			},
-//			false,
-//			nil,
-//		},
-//		"Send Error": {
-//			ResetPassword{
-//				Token: "token",
-//				User:  domain.UserPart{},
-//			},
-//			true,
-//			errors.GlobalError,
-//		},
-//		"Validation failed": {
-//			event{},
-//			true,
-//			"ResetPassword should be passed to dispatch",
-//		},
-//	}
-//
-//	for name, test := range tt {
-//		t.Run(name, func() {
-//			deps := t.Setup(test.error)
-//			dispatcher := NewResetPassword(deps)
-//			err := dispatcher.Dispatch(test.data, []string{"hello@verbiscms.com"}, nil)
-//			if err != nil {
-//				t.Contains(errors.Message(err), test.want)
-//				return
-//			}
-//			t.Equal(test.want, err)
-//		})
-//	}
-//}
+import (
+	"github.com/ainsleyclark/verbis/api/domain"
+	"github.com/ainsleyclark/verbis/api/errors"
+)
+
+func (t *EventTestSuite) Test_ResetPasswordDispatch() {
+	tt := map[string]struct {
+		data  interface{}
+		error bool
+		want  interface{}
+	}{
+		"Success": {
+			ResetPassword{
+				User: domain.UserPart{},
+				Url:  "token",
+			},
+			false,
+			nil,
+		},
+		"Validation failed": {
+			event{},
+			true,
+			"ResetPassword should be passed to dispatch",
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func() {
+			deps := t.Setup(test.error)
+			dispatcher := NewResetPassword(deps)
+			err := dispatcher.Dispatch(test.data, []string{"hello@verbiscms.com"}, nil)
+			if err != nil {
+				t.Contains(errors.Message(err), test.want)
+				return
+			}
+			t.Equal(test.want, err)
+		})
+	}
+}
