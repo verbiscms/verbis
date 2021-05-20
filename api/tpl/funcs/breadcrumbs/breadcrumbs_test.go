3cb1818cdcc78a167528bae25c1d9a3348f18f44
Ainsley Clark <info@ainsleyclark.com>
2021-05-20 20:28:04 +0100

    Working on breadcrumbs for frontend

--- api/tpl/funcs/breadcrumbs/breadcrumbs_test.go
+++ api/tpl/funcs/breadcrumbs/breadcrumbs_test.go
@@ -2,176 +2,23 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package posts
+package breadcrumbs
 
 import (
-	"fmt"
-	"github.com/ainsleyclark/verbis/api/deps"
-	"github.com/ainsleyclark/verbis/api/domain"
-	"github.com/ainsleyclark/verbis/api/errors"
-	"github.com/ainsleyclark/verbis/api/http/pagination"
-	mocks "github.com/ainsleyclark/verbis/api/mocks/store/posts"
-	"github.com/ainsleyclark/verbis/api/store"
-	"github.com/ainsleyclark/verbis/api/store/posts"
-	"github.com/ainsleyclark/verbis/api/test/dummy"
-	"github.com/ainsleyclark/verbis/api/tpl/params"
+	"github.com/ainsleyclark/verbis/api/verbis"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-var (
-	cat  = &domain.Category{}
-	post = domain.Post{
-		Id:     1,
-		Title:  "test title",
-		UserId: 1,
+func TestNamespace_Get(t *testing.T) {
+	c := verbis.Breadcrumbs{
+		Enabled: true,
+		Title:   "Items",
 	}
-	postData = domain.PostDatum{
-		Post:     post,
-		Author:   domain.UserPart{},
-		Category: cat,
+	ns := Namespace{
+		deps:   nil,
+		crumbs: c,
 	}
-	postDataSlice = domain.PostData{
-		postData, postData,
-	}
-	tplPost = domain.PostTemplate{
-		Author:   domain.UserPart{},
-		Category: cat,
-		Post:     post,
-	}
-	tplPostSlice = []domain.PostTemplate{
-		tplPost, tplPost,
-	}
-)
-
-type noStringer struct{}
-
-func Setup() (*Namespace, *mocks.Repository) {
-	mock := &mocks.Repository{}
-	return &Namespace{deps: &deps.Deps{
-		Store: &store.Repository{
-			Posts: mock,
-		},
-	}}, mock
-}
-
-func TestNamespace_Find(t *testing.T) {
-	tt := map[string]struct {
-		input interface{}
-		mock  func(m *mocks.Repository)
-		want  interface{}
-	}{
-		"Success": {
-			1,
-			func(m *mocks.Repository) {
-				m.On("Find", 1, false).Return(postData, nil)
-			},
-			tplPost,
-		},
-		"Not Found": {
-			1,
-			func(m *mocks.Repository) {
-				m.On("Find", 1, false).Return(domain.PostDatum{}, fmt.Errorf("error"))
-			},
-			nil,
-		},
-		"No Stringer": {
-			noStringer{},
-			func(m *mocks.Repository) {
-				m.On("Find", 1, false).Return(postData, nil)
-			},
-			nil,
-		},
-		"Nil": {
-			nil,
-			func(m *mocks.Repository) {
-				m.On("Find", 1, false).Return(postData, nil)
-			},
-			nil,
-		},
-	}
-
-	for name, test := range tt {
-		t.Run(name, func(t *testing.T) {
-			ns, mock := Setup()
-			test.mock(mock)
-			got := ns.Find(test.input)
-			assert.Equal(t, test.want, got)
-		})
-	}
-}
-
-func TestNamespace_List(t *testing.T) {
-	cfg := posts.ListConfig{
-		Resource: "",
-		Status:   "published",
-	}
-
-	tt := map[string]struct {
-		input params.Query
-		mock  func(m *mocks.Repository)
-		want  interface{}
-	}{
-		"Success": {
-			params.Query{"limit": 15},
-			func(m *mocks.Repository) {
-				m.On("List", dummy.DefaultParams, false, cfg).Return(postDataSlice, 5, nil)
-			},
-			Posts{
-				Posts: tplPostSlice,
-				Pagination: &pagination.Pagination{
-					Page:  1,
-					Pages: 1,
-					Limit: 15,
-					Total: 5,
-					Next:  false,
-					Prev:  false,
-				},
-			},
-		},
-		"Nil": {
-			nil,
-			func(m *mocks.Repository) {
-				m.On("List", dummy.DefaultParams, false, cfg).Return(postDataSlice, 5, nil)
-			},
-			Posts{
-				Posts: tplPostSlice,
-				Pagination: &pagination.Pagination{
-					Page:  1,
-					Pages: 1,
-					Limit: 15,
-					Total: 5,
-					Next:  false,
-					Prev:  false,
-				},
-			},
-		},
-		"Not Found": {
-			params.Query{"limit": 15},
-			func(m *mocks.Repository) {
-				m.On("List", dummy.DefaultParams, false, cfg).Return(nil, 0, &errors.Error{Code: errors.NOTFOUND, Message: "no posts found"})
-			},
-			nil,
-		},
-		"Internal Error": {
-			params.Query{"limit": 15},
-			func(m *mocks.Repository) {
-				m.On("List", dummy.DefaultParams, false, cfg).Return(nil, 0, &errors.Error{Code: errors.INTERNAL, Message: "config error"})
-			},
-			"config error",
-		},
-	}
-
-	for name, test := range tt {
-		t.Run(name, func(t *testing.T) {
-			ns, mock := Setup()
-			test.mock(mock)
-			got, err := ns.List(test.input)
-			if err != nil {
-				assert.Contains(t, err.Error(), test.want)
-				return
-			}
-			assert.Equal(t, test.want, got)
-		})
-	}
+	got := ns.Get()
+	assert.Equal(t, c, got)
 }
