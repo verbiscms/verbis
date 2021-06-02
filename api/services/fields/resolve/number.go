afdbfce03ad646fe45e2a49e5ad0af3865813038
Ainsley Clark <info@ainsleyclark.com>
2021-06-02 16:22:44 +0100

    Added float resolving to field number

--- api/services/fields/resolve/number.go
+++ api/services/fields/resolve/number.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ainsleyclark/verbis/api/domain"
 	"github.com/ainsleyclark/verbis/api/errors"
 	"github.com/spf13/cast"
+	"strings"
 )
 
 // number
@@ -15,12 +16,21 @@ import (
 // Casts the domain.FieldValue to int64.
 // Returns errors.INVALID if the domain.FieldValue could not be cast to an int64.
 func (v *Value) number(value domain.FieldValue) (interface{}, error) {
-	const op = "FieldResolver.number"
+	const op = "FieldResolver.Number"
 
-	number, err := cast.ToInt64E(value.String())
+	val := value.String()
+
+	if strings.Contains(val, ".") {
+		f, err := cast.ToFloat64E(val)
+		if err != nil {
+			return nil, &errors.Error{Code: errors.INVALID, Message: "Unable to cast field to an integer", Operation: op, Err: err}
+		}
+		return f, nil
+	}
+
+	n, err := cast.ToInt64E(val)
 	if err != nil {
 		return nil, &errors.Error{Code: errors.INVALID, Message: "Unable to cast field to an integer", Operation: op, Err: err}
 	}
-
-	return number, nil
+	return n, nil
 }
