fa22e463364f0e6704779b24e7ea5ea8dc276851
Ainsley Clark <info@ainsleyclark.com>
2021-06-17 19:50:53 +0100

    Tested domain formvalues & mime

--- api/domain/forms_test.go
+++ api/domain/forms_test.go
@@ -5,6 +5,9 @@
 package domain
 
 import (
+	"encoding/json"
+	"github.com/ainsleyclark/verbis/api/errors"
+	"github.com/spf13/cast"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -54,4 +57,80 @@ func TestFormLabel_Name(t *testing.T) {
 func TestFormLabel_String(t *testing.T) {
 	f := FormLabel("test")
 	assert.Equal(t, "test", f.String())
+}
+
+// TODO, Make scan and value test helper functions
+func TestFormValues_Scan(t *testing.T) {
+	tt := map[string]struct {
+		input interface{}
+		want  interface{}
+	}{
+		"Success": {
+			[]byte(`{"large": {"url": "/test"}}`),
+			nil,
+		},
+		"Bad Unmarshal": {
+			[]byte(`{"large": wrong}`),
+			"Error unmarshalling into FormValues",
+		},
+		"Nil": {
+			nil,
+			FormValues{},
+		},
+		"Unsupported Scan": {
+			"wrong",
+			"Scan unsupported for FormValues",
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func(t *testing.T) {
+			m := FormValues{}
+			err := m.Scan(test.input)
+			if err != nil {
+				assert.Contains(t, errors.Message(err), test.want)
+				return
+			}
+			assert.Nil(t, err)
+		})
+	}
+}
+
+func TestFormValues_Value(t *testing.T) {
+	tt := map[string]struct {
+		input FormValues
+		want  interface{}
+	}{
+		"Success": {
+			FormValues{
+				"key": "val",
+			},
+			FormValues{
+				"key": "val",
+			},
+		},
+		"Nil Length": {
+			nil,
+			nil,
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func(t *testing.T) {
+			value, _ := test.input.Value()
+
+			if test.input == nil {
+				assert.Nil(t, value)
+				return
+			}
+
+			got, err := cast.ToStringE(value)
+			assert.NoError(t, err)
+
+			want, err := json.Marshal(test.input)
+			assert.NoError(t, err)
+
+			assert.Equal(t, string(want), got)
+		})
+	}
 }
