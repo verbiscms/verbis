d1289a1c2728ee647149de13998c40a1410aad95
Ainsley Clark <info@ainsleyclark.com>
2021-06-23 08:25:22 +0100

    Updated release script

--- bin/release.sh
+++ bin/release.sh
@@ -4,33 +4,41 @@
 #
 
 # Set variables
-version=$1
+version=$( cat VERSION )
 message=$2
 
-# Check goreleaser passed
-goreleaser check
+echo $version
+exit
 
-# Check version is not empty
-if [[ $version == "" ]]
-  then
-    echo "Add Version number"
-    exit
+read -p "Have you updated VERSION file?" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   # Check goreleaser passed
+  goreleaser check
+
+  # Check version is not empty
+  if [[ $version == "" ]]
+    then
+      echo "Add Version number"
+      exit
+  fi
+
+  # Check commit message is not empty
+  if [[ $message == "" ]]
+    then
+      echo "Add commit message"
+      exit
+  fi
+
+  git tag -a $version -m $message
+  git push origin $version
+
+  # Run goreleaser
+  goreleaser release --rm-dist
 fi
 
-# Check commit message is not empty
-if [[ $message == "" ]]
-  then
-    echo "Add commit message"
-    exit
-fi
 
-rm -rf dist
-
-git tag -a $version -m $message
-git push origin $version
-
-# Run goreleaser
-goreleaser release --rm-dist
 
 
 
