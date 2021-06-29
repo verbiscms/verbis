b18454c92b0b317b31191a7a3f16e3c6fe5bc480
Ainsley Clark <info@ainsleyclark.com>
2021-06-29 17:46:15 +0100

    Working on storage provider

---
+++ api/domain/storage.go
@@ -0,0 +1,14 @@
+// Copyright 2020 The Verbis Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package domain
+
+type StorageProvider string
+
+const (
+	StorageLocal = StorageProvider("local")
+	StorageAWS   = StorageProvider("aws")
+	StorageGCP   = StorageProvider("google")
+	StorageAzure = StorageProvider("azure")
+)
