453c3f6f903c4c5cff65e3eeb298eb07729714af
ReddicoAinsley <60976884+ReddicoAinsley@users.noreply.github.com>
2021-07-16 18:23:20 +0100

    Tested domain

--- api/domain/storage_test.go
+++ api/domain/storage_test.go
@@ -9,6 +9,32 @@ import (
 	"testing"
 )
 
+func TestBuckets_IsValid(t *testing.T) {
+	tt := map[string]struct {
+		input string
+		buckets Buckets
+		want  bool
+	}{
+		"Valid": {
+			"bucket",
+			Buckets{Bucket{Name: "bucket-name", Id: "bucket"}},
+			true,
+		},
+		"Not Valid": {
+			"bucket",
+			Buckets{},
+			false,
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func(t *testing.T) {
+			got := test.buckets.IsValid(test.input)
+			assert.Equal(t, test.want, got)
+		})
+	}
+}
+
 func TestStorageProvider_String(t *testing.T) {
 	got := StorageLocal.String()
 	want := string(StorageLocal)
@@ -33,6 +59,37 @@ func TestStorageProvider_IsLocal(t *testing.T) {
 	for name, test := range tt {
 		t.Run(name, func(t *testing.T) {
 			got := test.input.IsLocal()
+			assert.Equal(t, test.want, got)
+		})
+	}
+}
+
+func TestStorageProvider_TitleCase(t *testing.T) {
+	tt := map[string]struct {
+		input StorageProvider
+		want  StorageProvider
+	}{
+		"Local": {
+			StorageLocal,
+			StorageProvider("Local"),
+		},
+		"AWS": {
+			StorageAWS,
+			StorageProvider("Aws"),
+		},
+		"Google": {
+			StorageGCP,
+			StorageProvider("Google"),
+		},
+		"Azure": {
+			StorageAzure,
+			StorageProvider("Azure"),
+		},
+	}
+
+	for name, test := range tt {
+		t.Run(name, func(t *testing.T) {
+			got := test.input.TitleCase()
 			assert.Equal(t, test.want, got)
 		})
 	}
