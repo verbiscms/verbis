60086109d2d7abbe0ba32016906c9be33d7f81ac
Ainsley Clark <info@ainsleyclark.com>
2021-07-19 19:14:21 +0100

    Fixed sys bug

--- api/sys/system.go
+++ api/sys/system.go
@@ -6,7 +6,6 @@ package sys
 
 import (
 	"github.com/ainsleyclark/verbis/api/database"
-	"github.com/ainsleyclark/verbis/api/errors"
 	"github.com/ainsleyclark/verbis/api/logger"
 	"github.com/ainsleyclark/verbis/api/version"
 	sm "github.com/hashicorp/go-version"
@@ -36,15 +35,9 @@ type Sys struct {
 // New creates a new system type, used for restarting
 // and manipulating the system.
 func New(db database.Driver) *Sys {
-	const op = "System.New"
-
 	exec, err := execPath()
 	if err != nil {
 		logger.Panic(err)
-	}
-
-	if err != nil {
-		logger.Panic(&errors.Error{Code: errors.INTERNAL, Message: "Error creating new Verbis updater", Operation: op, Err: err})
 	}
 
 	u := &updater.Updater{
