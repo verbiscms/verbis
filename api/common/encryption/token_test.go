808234f3965430b9f9e6bbf2f6ccbb8117a174e1
Ainsley Clark <info@ainsleyclark.com>
2021-07-23 11:15:52 +0100

    Finished housekeeping

---
+++ api/common/encryption/token_test.go
@@ -0,0 +1,15 @@
+// Copyright 2020 The Verbis Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package encryption
+
+//func TestGenerateUserToken(t *testing.T) {
+//	token := GenerateUserToken("name", "email")
+//
+//	color.Green.Println(len(token))
+//
+//	reg, err := regexp.MatchString("^[a-fA-F0-9]{32}$", token)
+//	assert.NoError(t, err)
+//	assert.True(t, reg)
+//}
