97ed1f06e344751d6595e5aa9e50d84c4d103da0
Ainsley Clark <info@ainsleyclark.com>
2021-08-27 12:38:11 +0100

    Updating proxy frontend

--- admin/src/views/intergrations/Proxies.vue
+++ admin/src/views/intergrations/Proxies.vue
@@ -14,116 +14,120 @@
 					</header>
 				</div><!-- /Col -->
 			</div><!-- /Row -->
-			<div class="row proxies">
+			<el-row class="row">
 				<!-- =====================
 					Details
 					===================== -->
 				<div class="col-12 col-desk-5">
-					<!-- Header -->
-					<div class="proxies-info-header">
-						<h2 class="mb-1">Reverse Proxies:</h2>
-						<p class="mb-2">A reverse proxy accepts a request from a client, forwards it to a server that can fulfill it, and returns the server’s response to the client. Verbis allows the use of reverse proxies to proxy traffic to external websites.</p>
-						<el-link href="https://verbiscms.com" target="_blank">Visit documentation</el-link>
-					</div>
-					<!-- Rewrites -->
-					<div class="proxies-info-rewrite">
-						<h3 class="mb-1">Rewrites</h3>
-						<p>Rewrite defines URL path rewrite rules. The values captured in asterisk can be retrieved by index e.g. $1, $2 and so on.</p>
-						<h6>Examples:</h6>
-						<el-table size="small" :data="rewriteExamples" border style="width: 100%; margin-top: 10px">
-							<el-table-column prop="from" label="From Path"></el-table-column>
-							<el-table-column prop="to" label="To Path"></el-table-column>
-						</el-table>
-					</div>
-					<!-- Regex Rewrites -->
-					<div class="proxies-info-rewrite">
-						<h3 class="mb-1">Regex Rewrites</h3>
-						<p>Regex Rewrites defines rewrite rules using regexp exppresions with captures. Every capture group in the values can be retrieved by index e.g. $1, $2 and so on.</p>
-						<h6>Examples:</h6>
-						<el-table size="small" :data="rewriteExamples" border style="width: 100%; margin-top: 10px">
-							<el-table-column prop="from" label="From Path"></el-table-column>
-							<el-table-column prop="to" label="To Path"></el-table-column>
-						</el-table>
-					</div>
+					<!-- Info -->
+					<div class="info">
+						<!-- Header -->
+						<div class="info-header">
+							<h2 class="mb-1">Reverse Proxies:</h2>
+							<p class="mb-2">A reverse proxy accepts a request from a client, forwards it to a server that
+								can fulfill it, and returns the server’s response to the client. Verbis allows the use of
+								reverse proxies to proxy traffic to external websites.</p>
+							<el-link href="https://verbiscms.com" target="_blank">Visit documentation</el-link>
+						</div>
+						<!-- Rewrites -->
+						<div class="info-rewrite">
+							<h3 class="mb-1">Rewrites</h3>
+							<p>Rewrite defines URL path rewrite rules. The values captured in asterisk can be retrieved by
+								index e.g. $1, $2 and so on.</p>
+							<h6>Examples:</h6>
+							<el-table size="small" :data="rewriteExamples" border style="width: 100%; margin-top: 10px">
+								<el-table-column prop="from" label="From Path"></el-table-column>
+								<el-table-column prop="to" label="To Path"></el-table-column>
+							</el-table>
+						</div>
+						<!-- Regex Rewrites -->
+						<div class="info-rewrite">
+							<h3 class="mb-1">Regex Rewrites</h3>
+							<p>Regex Rewrites defines rewrite rules using regexp exppresions with captures. Every capture
+								group in the values can be retrieved by index e.g. $1, $2 and so on.</p>
+							<h6>Examples:</h6>
+							<el-table size="small" :data="rewriteExamples" border style="width: 100%; margin-top: 10px">
+								<el-table-column prop="from" label="From Path"></el-table-column>
+								<el-table-column prop="to" label="To Path"></el-table-column>
+							</el-table>
+						</div>
+					</div><!-- /Info -->
 				</div><!-- /Col -->
 				<!-- =====================
 					Proxies
 					===================== -->
-				<div v-loading="doingAxios" class="col-12 col-desk-6 offset-desk-1">
-					<!-- Header -->
-					<div class="proxies-config-header">
-						<h2>Configuration</h2>
-						<el-button size="small" @click="showModal = true">New Proxy</el-button>
-					</div>
-					<el-alert class="mb-3" title="Order" type="warning" description="The order of which the proxies are defined are important for more info visit the link to the left." show-icon></el-alert>
-					<!-- Proxies -->
-					<draggable
-						class="proxies-draggable"
-						:class="{ 'proxies-draggable-dragging' : drag }"
-						v-if="proxies && proxies.length"
-						v-model="proxies"
-						@start="drag = true"
-						@end="drag = false">
-						<div v-for="(proxy, index) in proxies" :key="index" class="proxies-draggable-item">
-							<el-card class="proxies-card" shadow="never">
-								<!-- Header -->
-								<template #header>
-									<div class="proxies-config-item-header">
-										<span>{{ proxy.path }}</span>
-										<el-button-group>
-											<el-button size="small" icon="el-icon-edit" @click="editProxy(index)"></el-button>
-											<el-button size="small" icon="el-icon-rank"></el-button>
-											<el-popconfirm confirmButtonText="Yes" cancelButtonText="No" icon="el-icon-danger" iconColor="red" title="Are you sure to delete this proxy?" @confirm="deleteProxy(index)">
-												<template #reference>
-													<el-button size="small" icon="el-icon-delete"></el-button>
-												</template>
-											</el-popconfirm>
-										</el-button-group>
-									</div>
-								</template>
-								<!-- Path -->
-								<div class="proxies-config-item">
-									<h6>Path:</h6>
-									<p>{{ proxy.path }}</p>
-								</div>
-								<!-- Host -->
-								<div class="proxies-config-item">
-									<h6>Host:</h6>
-									<p>{{ proxy.host }}</p>
-								</div>
-								<!-- Rewrites -->
-								<div class="proxies-config-item">
-									<h6>Rewrites:</h6>
-									<el-table v-if="proxy.rewrites && proxy['rewrites'].length" size="mini" :data="proxy.rewrites" border style="width: 100%; margin-top: 10px">
-										<el-table-column prop="from" label="From Path"></el-table-column>
-										<el-table-column prop="to" label="To Path"></el-table-column>
-									</el-table>
-									<p v-else>No rewrites set</p>
-								</div><!-- /Rewrites -->
-								<!-- Regex -->
-								<div class="proxies-config-item">
-									<h6>Regex Rewrites:</h6>
-									<el-table v-if="proxy.rewrites && proxy['rewrites'].length" size="mini" :data="proxy.rewrites" border style="width: 100%; margin-top: 10px">
-										<el-table-column prop="from" label="From Path"></el-table-column>
-										<el-table-column prop="to" label="To Path"></el-table-column>
-									</el-table>
-									<p v-else>No rewrites set</p>
-								</div><!-- /Regex -->
-							</el-card>
+				<div class="col-12 col-desk-6 offset-desk-1">
+					<!-- Config -->
+					<div class="config">
+						<!-- Header -->
+						<div class="config-header">
+							<h2>Configuration</h2>
+							<el-button size="small" @click="newProxy">New Proxy</el-button>
 						</div>
-					</draggable><!-- /Proxies -->
+						<!-- Order Alert -->
+						<el-alert class="config-alert" title="Order" type="warning" description="The order of which the proxies are defined are important for more info visit the link to the left." show-icon></el-alert>
+					</div><!-- /Config -->
+					<!-- Proxies -->
+					<el-form :model="form" size="small" ref="proxiesForm" label-width="80px" label-position="left" v-if="proxies && proxies.length">
+						<el-collapse class="proxies" v-model="activeCollapse" accordion>
+							<draggable
+								class="proxies-draggable"
+								:class="{ 'proxies-draggable-dragging' : drag }"
+								v-model="proxies"
+								handle=".proxies-handle"
+								v-loading="doingAxios"
+								@start="handleDragStart"
+								@end="drag = false">
+								<el-collapse-item v-for="(proxy, index) in proxies" :key="index" :disabled="true" class="proxies-item" :name="proxy.name">
+									<!-- Header -->
+									<template slot="title">
+										<div class="proxies-header">
+											<span>{{ proxy.name }}</span>
+											<el-button-group class="proxies-header-btns">
+												<el-button size="mini" icon="el-icon-edit" @click="handleCollapse(proxy)"></el-button>
+												<el-button size="mini" icon="el-icon-rank" class="proxies-handle"></el-button>
+												<el-popconfirm confirmButtonText="Yes" cancelButtonText="No" icon="el-icon-danger" iconColor="red" title="Are you sure to delete this proxy?" @confirm="deleteProxy(index)">
+													<template #reference>
+														<el-button style="border-radius: 0 4px 4px 0;" size="mini" icon="el-icon-delete"></el-button>
+													</template>
+												</el-popconfirm>
+											</el-button-group>
+										</div>
+									</template><!-- /Header -->
+									<!-- Body -->
+									<div class="proxies-body">
+										<!-- Name -->
+										<el-form-item label="Name" prop="name" :rules="{ required: true, message: 'Enter a Name.', trigger: 'blur' }">
+											<el-input placeholder="Add a name" v-model="proxies[index].path"></el-input>
+										</el-form-item>
+										<!-- Path -->
+										<el-form-item label="Path" prop="path" :rules="{ required: true, message: 'Enter a Path.', trigger: 'blur' }">
+											<el-input placeholder="Add a path" v-model="proxies[index].path"></el-input>
+										</el-form-item>
+										<!-- Host -->
+										<el-form-item label="Host" prop="host" :rules="{ required: true, message: 'Enter a Host.', trigger: 'blur' }">
+											<el-input placeholder="Add a host" v-model="proxies[index].path"></el-input>
+										</el-form-item>
+										<!-- Rewrites -->
+										<el-form-item label="Rewrites" prop="rewrites">
+											<el-table v-if="proxy.rewrites && proxy['rewrites'].length" size="mini" :data="proxy.rewrites" border style="width: 100%; margin-top: 10px">
+												<el-table-column prop="from" label="From Path"></el-table-column>
+												<el-table-column prop="to" label="To Path"></el-table-column>
+											</el-table>
+											<p v-else>No rewrites set</p>
+										</el-form-item>
+									</div><!-- /Body -->
+								</el-collapse-item>
+							</draggable>
+						</el-collapse><!-- /Proxies -->
+					</el-form>
 					<el-empty v-else :image-size="100">
 						<h4>No proxies available</h4>
 						<p>Click the button above to create a new proxy</p>
 					</el-empty>
-
 				</div><!-- /Col -->
-			</div><!-- /Row -->
+			</el-row><!-- /Row -->
 		</div><!-- /Container -->
-		<!-- =====================
-			Create New Modal
-			===================== -->
-		<NewProxyModal :visible.sync="showModal" @create="createProxy"></NewProxyModal>
 	</section>
 </template>
 
@@ -133,9 +137,10 @@
 <script>
 
 import Breadcrumbs from "../../components/misc/Breadcrumbs";
-import NewProxyModal from "../../components/proxies/NewProxyModal";
 import {optionsMixin} from "@/util/options";
 import draggable from 'vuedraggable'
+
+const UNASSIGNED_PREFIX = "Unassigned";
 
 export default {
 	name: "Proxies",
@@ -144,16 +149,14 @@ export default {
 	components: {
 		Breadcrumbs,
 		draggable,
-		NewProxyModal,
 	},
 	data: () => ({
 		form: {},
 		rules: {
 			name: [
-				{ required: true, message: 'Enter link text for the menu item', trigger: 'blur' },
+				{required: true, message: 'Enter link text for the menu item', trigger: 'blur'},
 			],
 		},
-		showModal: false,
 		activeCollapse: "",
 		updatingProxy: {},
 		drag: false,
@@ -170,24 +173,26 @@ export default {
 	}),
 	created() {
 		setTimeout(() => {
-			this.proxies =  [
+			this.$set(this.data, 'proxies', [
 				{
+					"name": "Serp Speed",
 					"host": "https://35.214.23.223:5000/tools/serp-speed",
 					"path": "/tools/serp-speed",
-					"rewrites": {
-						"map/$1":"map/$3",
-					},
+					"rewrites": [
+						{from: "mappp", to: "ap/$"},
+					],
 					"rewrite_regex": [],
 				},
 				{
+					"name": "ReReDirect",
 					"host": "https://35.214.23.223:5000/tools/serp-speed",
 					"path": "/tools/reredirect",
 					"rewrites": {
-						"map/$1":"map/$3",
+						"map/$1": "map/$3",
 					},
 					"rewrite_regex": [],
 				}
-			]
+			]);
 		}, 500)
 	},
 	methods: {
@@ -197,17 +202,61 @@ export default {
 		 */
 		createProxy(proxy) {
 			this.proxies.push(proxy);
-			this.showModal = false;
 		},
+		/**
+		 * Deletes a reverse proxy from the array.
+		 */
 		deleteProxy(index) {
 			if (index !== -1) {
 				this.proxies.splice(index, 1);
 			}
 		},
-		editProxy(index) {
-			this.updatingProxy = this.proxies[index];
-			this.showModal = true;
-		}
+		/**
+		 * Creates a new proxy and adds it to
+		 * the array.
+		 */
+		newProxy() {
+			this.proxies.push({
+				name: this.getUnassignedName(),
+				path: "",
+				host: "",
+				rewrites: [],
+				regex_rewrites: [],
+			})
+		},
+		/**
+		 * Handles the accordion.
+		 */
+		handleCollapse(proxy) {
+			this.activeCollapse = proxy.name;
+		},
+		/**
+		 * Handle the start of a drag item, collapses
+		 * all accordion items.
+		 */
+		handleDragStart() {
+			this.activeCollapse = "";
+			this.drag = true;
+		},
+		/**
+		 * Retrieves the unassigned name for a proxy
+		 * when no name is set. It will incrmement
+		 * by one if none is found.
+		 */
+		getUnassignedName() {
+			if (!this.proxies.find(el => el.name === UNASSIGNED_PREFIX)) {
+				return UNASSIGNED_PREFIX;
+			}
+			let counter = 1;
+			// eslint-disable-next-line no-constant-condition
+			while (true) {
+				let found = this.proxies.find(el => el.name === UNASSIGNED_PREFIX + "-" + counter);
+				if (!found) {
+					return UNASSIGNED_PREFIX + "-" + counter;
+				}
+				counter++;
+			}
+		},
 	},
 	computed: {
 		/**
@@ -231,6 +280,50 @@ export default {
 	===================== -->
 <style scoped lang="scss">
 
+
+// Info
+// =========================================================================
+
+.info {
+
+	&-header {
+		margin-bottom: 2rem;
+	}
+
+	&-rewrite {
+		margin-bottom: 2.4rem;
+	}
+}
+
+// Config
+// =========================================================================
+
+
+.config {
+
+	&-header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-bottom: 1rem;
+
+		h2 {
+			margin-bottom: 0;
+		}
+	}
+
+	&-alert {
+		margin-bottom: 1rem;
+	}
+
+	::v-deep {
+
+		.el-alert__description {
+			margin-top: 0;
+		}
+	}
+}
+
 // Proxies
 // =========================================================================
 
@@ -246,67 +339,77 @@ export default {
 			display: none !important;
 		}
 
-		.el-alert__description {
-			margin-top: 0;
+		.el-collapse-item__header {
+			border-bottom: 0;
+			height: auto;
+			line-height: 0;
+
+			.el-collapse-item__arrow {
+				display: none;
+			}
+		}
+
+		.el-collapse-item__wrap {
+			border-bottom: 0;
+		}
+
+		.el-form-item__label {
+			color: $secondary;
+		}
+
+		.el-collapse-item.is-disabled .el-collapse-item__header {
+			cursor: default;
+		}
+
+	}
+
+	// Header
+	// =========================================================================
+
+	&-header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+		padding: 1rem;
+		line-height: 0;
+
+		span {
+			// TODO change to Element UI colour var
+			color: rgb(121, 187, 255);
+			font-weight: 600;
+			font-size: 0.9rem;
+		}
+
+		&-btns {
+			display: flex;
+			align-items: center;
 		}
 	}
 
-	// Config
+	// Body
 	// =========================================================================
 
-	&-config {
-
-		&-header {
-			display: flex;
-			align-items: center;
-			justify-content: space-between;
-			margin-bottom: 1rem;
-
-			h2 {
-				margin-bottom: 0;
-			}
-		}
-
-		&-item {
-			margin-bottom: 10px;
-			display: flex;
-			align-items: center;
-			justify-content: space-between;
-
-			p {
-				margin-bottom: 0;
-			}
-
-			&:last-child {
-				margin-bottom: 0;
-			}
-
-			&-header {
-				display: flex;
-				justify-content: space-between;
-				align-items: center;
-
-				span {
-					// TODO change to Element UI colour var
-					color: rgb(121, 187, 255);
-					font-weight: 600;
-					font-size: 0.9rem;
-				}
-			}
-		}
+	&-body {
+		padding: 0 1rem;
 	}
 
-	// Info
+	// Handle
 	// =========================================================================
 
-	&-info {
+	&-handle {
+		cursor: all-scroll;
+	}
 
-		&-header {
-			margin-bottom: 2rem;
-		}
+	// Item
+	// =========================================================================
 
-		&-rewrite {
-			margin-bottom: 2.4rem;
+	&-item {
+		border: 1px solid rgba(#DCDFE6, 0.5);
+		border-bottom: none;
+
+		&:last-child {
+			border-bottom: 1px solid rgba(#DCDFE6, 0.5);
 		}
 	}
 
@@ -319,13 +422,8 @@ export default {
 		border: 1px dashed rgba($grey, 0.3);
 		border-radius: 4px;
 
-		&-item {
-			margin-bottom: 1rem;
-		}
-
 		&-dragging {
 			// TODO change to Element UI colour var
-
 			border: 1px dashed #409EFF;
 			cursor: pointer;
 
